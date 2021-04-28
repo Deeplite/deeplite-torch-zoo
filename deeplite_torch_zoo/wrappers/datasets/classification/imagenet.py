a2c40fa0db52109aab3bba3f8b4dd97e877f34e8
Ahmed <ahmed@deeplite.ai>
2021-04-28 17:47:53 -0500

    revert changes to imagenet loaders

--- deeplite_torch_zoo/wrappers/datasets/classification/imagenet.py
+++ deeplite_torch_zoo/wrappers/datasets/classification/imagenet.py
@@ -8,30 +8,30 @@ from torch.utils.data.dataloader import default_collate
 __all__ = ["get_imagenet"]
 
 
-def get_imagenet(data_root="", train_dir="imagenet_training", val_dir="imagenet_val", batch_size=128, num_workers=4, device="cuda", **kwargs):
+def get_imagenet(data_root="", batch_size=128, num_workers=4, device="cuda", **kwargs):
     def assign_device(x):
-        #if device == "cuda":
-        #    return x
+        if device == "cuda":
+            return x
         return [v.to(device) for v in x]
 
     train_dataset = datasets.ImageFolder(
-        os.path.join(data_root, train_dir),
+        os.path.join(data_root, "imagenet_training"),
         transforms.Compose(
             [
-                transforms.RandomResizedCrop(64),
+                transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                transforms.Normalize(mean=[0.4802, 0.4481, 0.3975], std=[0.2302, 0.2265, 0.2262]),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ]
         ),
     )
 
     test_dataset = datasets.ImageFolder(
-        os.path.join(data_root, val_dir),
+        os.path.join(data_root, "imagenet_val"),
         transforms.Compose(
             [
-                transforms.Resize(64),
-                #transforms.CenterCrop(224),
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ]
@@ -42,7 +42,7 @@ def get_imagenet(data_root="", train_dir="imagenet_training", val_dir="imagenet_
         train_dataset,
         batch_size=batch_size,
         shuffle=True,
-        #pin_memory=True,
+        pin_memory=True,
         num_workers=num_workers,
         collate_fn=lambda x: assign_device(default_collate(x)),
     )
@@ -51,7 +51,7 @@ def get_imagenet(data_root="", train_dir="imagenet_training", val_dir="imagenet_
         test_dataset,
         batch_size=batch_size,
         shuffle=False,
-        #pin_memory=True,
+        pin_memory=True,
         num_workers=num_workers,
         collate_fn=lambda x: assign_device(default_collate(x)),
     )
