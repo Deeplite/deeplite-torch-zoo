0bba40fc1cb03bf4a2d710f1636d74c6a624fe5e
ahmed <ahmed@deeplite.ai>
2021-06-02 14:43:34 -0400

    add distributed training for yolo3 training script

---
+++ deeplite_torch_zoo/src/objectdetection/dist_train.sh
@@ -0,0 +1,9 @@
+GPUS=$1
+ARCH=$2
+DATASET=$3
+DATAROOT=$4
+PORT=${PORT:-29507}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/train_dp.py --world_size $GPUS --net $ARCH --dataset $DATASET --img-dir $DATAROOT --launcher pytorch ${@:6}
