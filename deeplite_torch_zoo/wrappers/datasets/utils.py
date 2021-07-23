a4758ed86710cfe0c91dd2f607b5978a36212034
ahmed <ahmed@deeplite.ai>
2021-07-23 08:03:36 -0400

    make sure get dataloader is picklable because otherwise it fails with distributed training when num_workers > 0

--- deeplite_torch_zoo/wrappers/datasets/utils.py
+++ deeplite_torch_zoo/wrappers/datasets/utils.py
@@ -4,6 +4,12 @@ from torch.utils.data.dataloader import default_collate
 from torch.utils.data.distributed import DistributedSampler as DS
 
 
+def half_precision(x):
+    x = collate_fn(x)
+    x = [_x.half() if isinstance(_x, torch.FloatTensor) else _x for _x in x]
+    return x
+
+
 def get_dataloader(
     dataset, batch_size=32, num_workers=4, fp16=False, distributed=False, shuffle=False,
     collate_fn=None, device="cuda"
@@ -11,17 +17,12 @@ def get_dataloader(
     if collate_fn is None:
         collate_fn = default_collate
 
-    def half_precision(x):
-        if fp16:
-            x = [_x.half() if isinstance(_x, torch.FloatTensor) else _x for _x in x]
-        return x
-
     dataloader = torch.utils.data.DataLoader(
         dataset,
         batch_size=batch_size,
         shuffle=shuffle,
         num_workers=num_workers,
-        collate_fn=lambda x: half_precision(collate_fn(x)),
+        collate_fn= half_precision if fp16 else collate_fn,
         sampler=DS(dataset) if distributed else None,
     )
     return dataloader
