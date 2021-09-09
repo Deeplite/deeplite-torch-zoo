ac492ba282b7426fc46585b3c4332704ebf76141
werner <werner@ibm6.deeplite-inc.cloud>
2021-09-09 13:49:40 -0500

    added yolo4 large leaky model definition

--- deeplite_torch_zoo/src/objectdetection/yolov5/models/common.py
+++ deeplite_torch_zoo/src/objectdetection/yolov5/models/common.py
@@ -3,6 +3,7 @@ import math
 
 import torch
 import torch.nn as nn
+from torch.nn.modules.activation import LeakyReLU
 
 from deeplite_torch_zoo.src.objectdetection.yolov5.utils.activations import \
     Hardswish
@@ -156,6 +157,31 @@ class SPPCSP(nn.Module):
         self.cv6 = Conv(c_, c_, 3, 1)
         self.bn = nn.BatchNorm2d(2 * c_)
         self.act = Mish()
+        self.cv7 = Conv(2 * c_, c2, 1, 1)
+
+    def forward(self, x):
+        x1 = self.cv4(self.cv3(self.cv1(x)))
+        y1 = self.cv6(self.cv5(torch.cat([x1] + [m(x1) for m in self.m], 1)))
+        y2 = self.cv2(x)
+        return self.cv7(self.act(self.bn(torch.cat((y1, y2), dim=1))))
+
+
+class SPPCSPLeaky(nn.Module):
+    # CSP SPP https://github.com/WongKinYiu/CrossStagePartialNetworks
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5, k=(5, 9, 13)):
+        super(SPPCSP, self).__init__()
+        c_ = int(2 * c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = nn.Conv2d(c1, c_, 1, 1, bias=False)
+        self.cv3 = Conv(c_, c_, 3, 1)
+        self.cv4 = Conv(c_, c_, 1, 1)
+        self.m = nn.ModuleList(
+            [nn.MaxPool2d(kernel_size=x, stride=1, padding=x // 2) for x in k]
+        )
+        self.cv5 = Conv(4 * c_, c_, 1, 1)
+        self.cv6 = Conv(c_, c_, 3, 1)
+        self.bn = nn.BatchNorm2d(2 * c_)
+        self.act = LeakyReLU(negative_slope=0.1)
         self.cv7 = Conv(2 * c_, c2, 1, 1)
 
     def forward(self, x):
