e8c1ab40a2b48e8596bb1267eb6d22bf110cf46e
Werner Chao <werner@deeplite.ai>
2021-09-28 14:56:36 -0500

    Bump version to 1.0.6

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ setup(
         'sdist': gitcmd_sdist,
     },
     name="deeplite-torch-zoo",
-    version="1.0.5",
+    version="1.0.6",
     description="The deeplite-torch-zoo package is a collection of popular pretrained deep learning models and their datasets for PyTorch framework.",
     long_description=long_description,
     long_description_content_type="text/markdown",
