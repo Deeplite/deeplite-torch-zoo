468c5c2a4241c66356ab83b1a4e57a6e679a0304
ahmed <ahmed@deeplite.ai>
2021-10-04 19:05:19 -0500

    add eval and dataloader for yolo_widerface

---
+++ deeplite_torch_zoo/src/objectdetection/datasets/wider_face.py
@@ -0,0 +1,189 @@
+import os
+import pathlib
+import cv2
+import numpy as np
+import torch
+from os.path import abspath, expanduser
+from typing import Any, Callable, List, Dict, Optional, Tuple, Union
+from deeplite_torch_zoo.src.objectdetection.yolov3.utils.data_augment import (
+    Mixup, RandomAffine, RandomCrop, RandomHorizontalFilp, Resize)
+
+
+WF_CLASS_NAMES = {
+    "BACKGROUND",
+    "face",
+}
+
+
+class WiderFace:
+    def __init__(self, root, split="train", num_classes=None, img_size=416):
+        """`WIDERFace <http://shuoyang1213.me/WIDERFACE/>`_ Dataset.
+        Args:
+            root (string): Root directory where images and annotations are downloaded to.
+                Expects the following folder structure:
+                .. code::
+                    <root>
+                        └── widerface
+                            ├── wider_face_split 
+                            ├── WIDER_train
+                            ├── WIDER_val 
+                            └── WIDER_test
+            split (string): The dataset split to use. One of {``train``, ``val``, ``test``}.
+                Defaults to ``train``.
+
+        """
+        self.root = pathlib.Path(root)
+        self.split = split
+        self.img_info: List[Dict[str, Union[str, Dict[str, torch.Tensor]]]] = []
+
+        if self.split == "test":
+            self.parse_test_annotations_file()
+        else:
+            self.parse_train_val_annotations_file()
+
+        print(f"{split}: {len(self.img_info)}")
+        self.classes = WF_CLASS_NAMES
+        self.num_classes = len(self.classes)
+        if num_classes is not None:
+            self.num_classes = num_classes
+        self._img_size = img_size
+
+    def __getitem__(self, item):
+        """
+        return:
+            bboxes of shape nx6, where n is number of labels in the image and x1,y1,x2,y2, class_id and confidence.
+        """
+        img_org, bboxes_org, img_id = self.__parse_annotation(self.__annotations[item])
+        img_org = img_org.transpose(2, 0, 1)  # HWC->CHW
+
+        item_mix = random.randint(0, len(self.__annotations) - 1)
+        img_mix, bboxes_mix, _ = self.__parse_annotation(self.__annotations[item_mix])
+        img_mix = img_mix.transpose(2, 0, 1)
+
+        img, bboxes = Mixup()(img_org, bboxes_org, img_mix, bboxes_mix)
+        del img_org, bboxes_org, img_mix, bboxes_mix
+
+        img = torch.from_numpy(img).float()
+
+        bboxes = torch.from_numpy(bboxes).float()
+
+        return img, bboxes, bboxes.shape[0], img_id
+
+    def __parse_annotation(self, annotation):
+        """
+        Data augument.
+        :param annotation: Image' path and bboxes' coordinates, categories.
+        ex. [image_path xmin,ymin,xmax,ymax,class_ind xmin,ymin,xmax,ymax,class_ind ...]
+        :return: Return the enhanced image and bboxes. bbox'shape is [xmin, ymin, xmax, ymax, class_ind]
+        """
+
+        _info = self.img_info[index]
+
+        img_path = _info["img_path"]
+        img = cv2.imread(img_path)  # H*W*C and C=BGR
+        assert img is not None, "File Not Found " + img_path
+
+        bboxes = _info["annotations"]["bbox"]
+        labels = np.ones(len(bboxes))
+        bboxes = np.concatenate((bboxes, labels), axis=1)
+
+        if len(bboxes) == 0:
+            bboxes = np.array(np.zeros((0, 5)))
+        else:
+            img, bboxes = RandomHorizontalFilp()(np.copy(img), np.copy(bboxes))
+            img, bboxes = RandomCrop()(np.copy(img), np.copy(bboxes))
+            img, bboxes = RandomAffine()(np.copy(img), np.copy(bboxes))
+        img, bboxes = Resize((self._img_size, self._img_size), True)(
+            np.copy(img), np.copy(bboxes)
+        )
+        return img, bboxes, str(Path(img_path).stem)
+
+    def collate_img_label_fn(self, sample):
+        images = []
+        labels = []
+        lengths = []
+        labels_with_tail = []
+        max_num_obj = 0
+        for image, label, length, img_id in sample:
+            images.append(image)
+            labels.append(label)
+            lengths.append(length)
+            max_num_obj = max(max_num_obj, length)
+        for label in labels:
+            num_obj = label.size(0)
+            zero_tail = torch.zeros(
+                (max_num_obj - num_obj, label.size(1)),
+                dtype=label.dtype,
+                device=label.device,
+            )
+            label_with_tail = torch.cat((label, zero_tail), dim=0)
+            labels_with_tail.append(label_with_tail)
+        image_tensor = torch.stack(images)
+        label_tensor = torch.stack(labels_with_tail)
+        length_tensor = torch.tensor(lengths)
+        return image_tensor, label_tensor, length_tensor, None
+
+    def __len__(self) -> int:
+        return len(self.img_info)
+
+    def _parse_file_name(self, line):
+        line = line.rstrip()
+        img_path = os.path.join(self.root, "WIDER_" + self.split, "images", line)
+        img_path = abspath(expanduser(img_path))
+        return img_path
+
+    def parse_train_val_annotations_file(self) -> None:
+        filename = "wider_face_train_bbx_gt.txt" if self.split == "train" else "wider_face_val_bbx_gt.txt"
+        filepath = os.path.join(self.root, "wider_face_split", filename)
+
+        with open(filepath, "r") as f:
+            lines = f.readlines()
+            i = 0
+            while i < len(lines):
+                labels = []
+                img_path = self._parse_file_name(lines[i])
+                _num_boxes = int(lines[i+1].rstrip())
+                num_boxes = max(1, _num_boxes)
+                i = i + 2
+                bboxes_lines = lines[i: i+num_boxes]
+                i = i + num_boxes
+                for bbox_line in bboxes_lines:
+                    line_split = bbox_line.rstrip().split(" ")
+                    bbox_values = np.array([float(x) for x in line_split])
+
+                    # Tiny faces cause inf loss for bboxes regression loss
+                    if bbox_values[2] < 20 or bbox_values[3] < 20:
+                        continue
+                    bbox_values[2:4] += bbox_values[0:2]
+                    labels.append(bbox_values)
+
+                if len(labels) == 0:
+                    continue
+
+                labels = np.array(labels)
+                _invalid = labels[:, 7]
+                valid_boxes = num_boxes - sum(_invalid)
+                if _num_boxes == 0 or valid_boxes <= 0:
+                    continue
+
+                self.img_info.append({
+                    "img_path": img_path,
+                    "annotations": {"bbox": labels[:, 0:4],  # xmin, ymin, xmax, ymax
+                                    "blur": labels[:, 4],
+                                    "expression": labels[:, 5],
+                                    "illumination": labels[:, 6],
+                                    "invalid": _invalid,
+                                    "occlusion": labels[:, 8],
+                                    "pose": labels[:, 9]}
+                })
+
+    def parse_test_annotations_file(self) -> None:
+        filepath = os.path.join(self.root, "wider_face_split", "wider_face_test_filelist.txt")
+        filepath = abspath(expanduser(filepath))
+        with open(filepath, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.rstrip()
+                img_path = os.path.join(self.root, "WIDER_test", "images", line)
+                img_path = abspath(expanduser(img_path))
+                self.img_info.append({"img_path": img_path})
---
+++ deeplite_torch_zoo/src/objectdetection/eval/wider_face_eval.py
@@ -0,0 +1,96 @@
+# Copyright (c) 2018-2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import glob
+import time
+
+import cv2
+import numpy as np
+import torch
+
+import deeplite_torch_zoo.src.objectdetection.configs.lisa_config as lisa_cfg
+from deeplite_torch_zoo.src.objectdetection.datasets.wider_face import WiderFace
+from deeplite_torch_zoo.src.objectdetection.eval.evaluator import Evaluator
+from deeplite_torch_zoo.src.objectdetection.eval.metrics import MAP
+from deeplite_torch_zoo.src.objectdetection.yolov3.utils.tools import post_process
+
+
+
+class WiderFaceEval(Evaluator):
+    """docstring for WiderFaceEval"""
+
+    def __init__(self, model, data_root, net="yolov3", img_size=448):
+        data_path = "deeplite_torch_zoo/results/wider_face/{net}".format(net=net)
+        super(WiderFaceEval, self).__init__(
+            model=model, data_path=data_path, img_size=img_size, net=net
+        )
+
+        self.dataset = WiderFace(data_root, _set="valid")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def evaluate(self):
+
+        self.model.eval()
+        self.model.cuda()
+        results = []
+        start = time.time()
+        for img_idx, _ in enumerate(self.dataset.img_info):
+            _info = self.dataset.img_info[img_idx]
+            img_path = _info["img_path"]
+            image = cv2.imread("{}/{}".format(img_path))
+            
+            print("Parsing batch: {}/{}".format(img_idx, len(self.dataset)), end="\r")
+            bboxes_prd = self.get_bbox(image)
+            if len(bboxes_prd) == 0:
+                bboxes_prd = np.zeros((0, 6))
+
+            # Handle the batch of predictions produced
+            # This is slow, but consistent with old implementation.
+            detections = {"bboxes": [], "labels": []}
+            detections["bboxes"] = bboxes_prd[:, :5]
+            detections["labels"] = bboxes_prd[:, 5]
+
+            gt_bboxes = _info["annotations"]["bbox"]
+            gt_labels = np.ones(len(gt_bboxes))
+            gt = {"bboxes": gt_bboxes, "labels": gt_labels}
+            results.append({"detections": detections, "gt": gt})
+        # put your model in training mode back on
+
+        mAP = MAP(results, self.dataset.num_classes)
+        mAP.evaluate()
+        ap = mAP.accumlate()
+        mAP_all_classes = np.mean(ap)
+
+        for i in range(0, ap.shape[0]):
+            print("{:_>25}: {:.3f}".format(self.dataset.inv_map[i], ap[i]))
+
+        print("(All Classes) AP = {:.3f}".format(np.mean(ap)))
+
+        ap = ap[ap > 1e-6]
+        ap = np.mean(ap)
+        print("(Selected) AP = {:.3f}".format(ap))
+
+        return ap  # Average Precision  (AP) @[ IoU=050 ]
+
+
+def yolo_eval_wider_face(model, data_root, device="cuda", net="yolov3", img_size=448, **kwargs):
+
+    mAP = 0
+    result = {}
+    model.to(device)
+    with torch.no_grad():
+        mAP = WiderFaceEval(model, data_root, net=net, img_size=img_size).evaluate()
+        result["mAP"] = mAP
+
+    return result
