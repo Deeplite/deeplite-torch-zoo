34aded8305b8130a6afb3f9e2c9b68ce36b7cf27
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2022-01-13 16:22:04 -0500

    refactor torchvision wrappers

--- deeplite_torch_zoo/wrappers/models/classification/imagenet/torchvision_models.py
+++ deeplite_torch_zoo/wrappers/models/classification/imagenet/torchvision_models.py
@@ -1,155 +1,55 @@
-from torchvision import models
+import torchvision
 
-def resnet18_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.resnet18(pretrained=pretrained)
-    return model.to(device)
-
-
-def resnet34_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.resnet34(pretrained=pretrained)
-    return model.to(device)
-
-
-def resnet50_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.resnet50(pretrained=pretrained)
-    return model.to(device)
-
-
-def resnet101_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.resnet101(pretrained=pretrained)
-    return model.to(device)
-
-
-def resnet152_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.resnet152(pretrained=pretrained)
-    return model.to(device)
-
-
-def alexnet_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.alexnet(pretrained=pretrained)
-    return model.to(device)
-
-
-def vgg16_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.vgg16(pretrained=pretrained)
-    return model.to(device)
-
-
-def squeezenet1_0_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.squeezenet1_0(pretrained=pretrained)
-    return model.to(device)
-
-
-def squeezenet1_1_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.squeezenet1_1(pretrained=pretrained)
-    return model.to(device)
+MODEL_NAMES = [
+    "alexnet",
+    "resnet18",
+    "resnet34",
+    "resnet50",
+    "resnet101",
+    "resnet152",
+    "resnext50_32x4d",
+    "resnext101_32x8d",
+    "wide_resnet50_2",
+    "wide_resnet101_2",
+    "vgg11",
+    "vgg11_bn",
+    "vgg13",
+    "vgg13_bn",
+    "vgg16",
+    "vgg16_bn",
+    "vgg19_bn",
+    "vgg19",
+    "squeezenet1_0",
+    "squeezenet1_1",
+    "inception_v3",
+    "densenet121",
+    "densenet169",
+    "densenet201",
+    "densenet161",
+    "googlenet",
+    "mobilenet_v2",
+    "mobilenet_v3_large",
+    "mobilenet_v3_small",
+    "mnasnet0_5",
+    "mnasnet0_75",
+    "mnasnet1_0",
+    "mnasnet1_3",
+    "shufflenet_v2_x0_5",
+    "shufflenet_v2_x1_0",
+    "shufflenet_v2_x1_5",
+    "shufflenet_v2_x2_0",
+]
 
 
-def densenet121_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.densenet121(pretrained=pretrained)
-    return model.to(device)
+def make_wrapper_func(wrapper_fn_name, model_name_key):
+    def wrapper_func(pretrained=False, progress=True, device="cuda"):
+        model = torchvision.models.__dict__[model_name_key](pretrained=pretrained)
+        return model.to(device)
+
+    wrapper_func.__name__ = wrapper_fn_name
+    return wrapper_func
 
 
-def densenet161_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.densenet161(pretrained=pretrained)
-    return model.to(device)
-
-
-def densenet169_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.densenet169(pretrained=pretrained)
-    return model.to(device)
-
-
-def densenet201_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.densenet201(pretrained=pretrained)
-    return model.to(device)
-
-
-def googlenet_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.googlenet(pretrained=pretrained)
-    return model.to(device)
-
-
-def shufflenet_v2_x0_5_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.shufflenet_v2_x0_5(pretrained=pretrained)
-    return model.to(device)
-
-
-def shufflenet_v2_x1_0_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.shufflenet_v2_x1_0(pretrained=pretrained)
-    return model.to(device)
-
-
-def mobilenet_v2_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.mobilenet_v2(pretrained=pretrained)
-    return model.to(device)
-
-
-def resnext50_32x4d_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.resnext50_32x4d(pretrained=pretrained)
-    return model.to(device)
-
-
-def resnext101_32x8d_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.resnext101_32x8d(pretrained=pretrained)
-    return model.to(device)
-
-
-def wide_resnet50_2_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.wide_resnet50_2(pretrained=pretrained)
-    return model.to(device)
-
-
-def wide_resnet101_2_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.wide_resnet101_2(pretrained=pretrained)
-    return model.to(device)
-
-
-def mnasnet1_0_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.mnasnet1_0(pretrained=pretrained)
-    return model.to(device)
-
-
-def mnasnet0_5_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.mnasnet0_5(pretrained=pretrained)
-    return model.to(device)
-
-
-def vgg11_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.vgg11(pretrained=pretrained)
-    return model.to(device)
-
-
-def vgg11_bn_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.vgg11_bn(pretrained=pretrained)
-    return model.to(device)
-
-
-def vgg13_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.vgg13(pretrained=pretrained)
-    return model.to(device)
-
-
-def vgg13_bn_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.vgg13_bn(pretrained=pretrained)
-    return model.to(device)
-
-
-def vgg16_bn_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.vgg16_bn(pretrained=pretrained)
-    return model.to(device)
-
-
-def vgg19_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.vgg19(pretrained=pretrained)
-    return model.to(device)
-
-
-def vgg19_bn_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.vgg19_bn(pretrained=pretrained)
-    return model.to(device)
-
-
-def inception_v3_imagenet(pretrained=False, progress=True, device="cuda"):
-    model = models.inception_v3(pretrained=pretrained)
-    return model.to(device)
+for model_name_tag in MODEL_NAMES:
+    wrapper_name = "_".join((model_name_tag, "imagenet"))
+    globals()[wrapper_name] = make_wrapper_func(wrapper_name, model_name_tag)
