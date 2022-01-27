cea4cd725a71fde5b5185c1626bb6c2ed924fdfe
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2022-01-27 16:55:02 -0500

    update dependencies

--- deeplite_torch_zoo/wrappers/models/classification/imagenet/timm_models.py
+++ deeplite_torch_zoo/wrappers/models/classification/imagenet/timm_models.py
@@ -1,10 +1,8 @@
 import timm
+from deeplite_torch_zoo.wrappers.models.classification.imagenet.torchvision_models import MODEL_NAMES
 
 
-MODEL_NAMES = [
-    'cspdarknet53',
-    'cspresnet50'
-]
+TIMM_MODEL_NAMES = set(timm.list_models()) - set(MODEL_NAMES)
 
 def make_wrapper_func(wrapper_fn_name, model_name_key):
     def wrapper_func(pretrained=False, progress=True, device="cuda"):
@@ -15,6 +13,6 @@ def make_wrapper_func(wrapper_fn_name, model_name_key):
     return wrapper_func
 
 
-for model_name_tag in MODEL_NAMES:
+for model_name_tag in TIMM_MODEL_NAMES:
     wrapper_name = "_".join((model_name_tag, "imagenet"))
     globals()[wrapper_name] = make_wrapper_func(wrapper_name, model_name_tag)
