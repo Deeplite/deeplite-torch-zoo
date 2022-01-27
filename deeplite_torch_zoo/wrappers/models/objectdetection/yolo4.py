3855872a8353026fdcc398f99f15a39014c10a09
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2022-01-26 21:47:44 -0500

    define yolov3 models thru the yolo5_6 model class

--- deeplite_torch_zoo/wrappers/models/objectdetection/yolo4.py
+++ deeplite_torch_zoo/wrappers/models/objectdetection/yolo4.py
@@ -1,14 +1,16 @@
 import re
+import urllib.parse as urlparse
 from pathlib import Path
 from collections import namedtuple
 
+import deeplite_torch_zoo
 from deeplite_torch_zoo.src.objectdetection.yolov5.models.yolov5 import YoloV5
 from deeplite_torch_zoo.src.objectdetection.yolov5.models.yolov5_6 import YoloV5_6
 from deeplite_torch_zoo.wrappers.models.utils import load_pretrained_weights
 
 
 def get_project_root() -> Path:
-    return Path(__file__).parent.parent.parent.parent.parent
+    return Path(deeplite_torch_zoo.__file__).parents[1]
 
 
 __all__ = [
@@ -17,25 +19,28 @@ __all__ = [
     "YOLOV4_MODELS",
 ]
 
+CFG_PATH = "deeplite_torch_zoo/src/objectdetection/configs/model_configs"
+CHECKPOINT_STORAGE_URL = "http://download.deeplite.ai/zoo/models"
+
 model_urls = {
-    "yolov4s_voc_20": "http://download.deeplite.ai/zoo/models/yolo4s-voc-20classes_850-270ddc5d43290a95.pth",
-    "yolov4m_voc_20": "http://download.deeplite.ai/zoo/models/yolo4m-voc-20classes_885-b854caad9ca7fb7c.pth",
-    "yolov4l_voc_20": "http://download.deeplite.ai/zoo/models/yolo4l-voc-20classes_872-9f54132ce2934fbf.pth",
-    "yolov4x_voc_20": "http://download.deeplite.ai/zoo/models/yolo4x-voc-20classes_882-187f352b9d0d29c6.pth",
-    "yolov4m_lisa_11": "http://download.deeplite.ai/zoo/models/yolov4m-lisa_11_880-6615c5e27557fab0.pth",
-    "yolov4l_leaky_voc_20": "http://download.deeplite.ai/zoo/models/yolo4l-leaky-voc-20classes_891-2c0f78ee3938ade3.pt",
-    "yolov4_6s_coco_80": "http://download.deeplite.ai/zoo/models/yolov4_6s-coco-80classes-288_b112910223d6c56d.pt",
-    "yolov4_6m_coco_80": "http://download.deeplite.ai/zoo/models/yolov4_6m-coco-80classes-309_02b2013002a4724b.pt",
+    "yolov4s_voc_20": "yolo4s-voc-20classes_850-270ddc5d43290a95.pth",
+    "yolov4m_voc_20": "yolo4m-voc-20classes_885-b854caad9ca7fb7c.pth",
+    "yolov4l_voc_20": "yolo4l-voc-20classes_872-9f54132ce2934fbf.pth",
+    "yolov4x_voc_20": "yolo4x-voc-20classes_882-187f352b9d0d29c6.pth",
+    "yolov4m_lisa_11": "yolov4m-lisa_11_880-6615c5e27557fab0.pth",
+    "yolov4l_leaky_voc_20": "yolo4l-leaky-voc-20classes_891-2c0f78ee3938ade3.pt",
+    "yolov4_6s_coco_80": "yolov4_6s-coco-80classes-288_b112910223d6c56d.pt",
+    "yolov4_6m_coco_80": "yolov4_6m-coco-80classes-309_02b2013002a4724b.pt",
 }
 
 yolov4_cfg = {
-    "yolov4s": "deeplite_torch_zoo/src/objectdetection/configs/model_configs/yolov4s-mish.yaml",
-    "yolov4m": "deeplite_torch_zoo/src/objectdetection/configs/model_configs/yolov4m-mish.yaml",
-    "yolov4l": "deeplite_torch_zoo/src/objectdetection/configs/model_configs/yolov4l-mish.yaml",
-    "yolov4x": "deeplite_torch_zoo/src/objectdetection/configs/model_configs/yolov4x-mish.yaml",
-    "yolov4l_leaky": "deeplite_torch_zoo/src/objectdetection/configs/model_configs/yolov4l-leaky.yaml",
-    "yolov4_6s": "deeplite_torch_zoo/src/objectdetection/configs/model_configs/yolov4s-mish.yaml",
-    "yolov4_6m": "deeplite_torch_zoo/src/objectdetection/configs/model_configs/yolov4m-mish.yaml",
+    "yolov4s": "yolov4s-mish.yaml",
+    "yolov4m": "yolov4m-mish.yaml",
+    "yolov4l": "yolov4l-mish.yaml",
+    "yolov4x": "yolov4x-mish.yaml",
+    "yolov4l_leaky": "yolov4l-leaky.yaml",
+    "yolov4_6s": "yolov4s-mish.yaml",
+    "yolov4_6m": "yolov4m-mish.yaml",
 }
 
 YOLOV4_MODELS = list(yolov4_cfg.keys())
@@ -45,10 +50,10 @@ def yolo4(
     net="yolov4s", _set_classes="voc_20", num_classes=20, pretrained=False,
     progress=True, device="cuda",
 ):
-    config_path = get_project_root() / yolov4_cfg[net]
+    config_path = get_project_root() / CFG_PATH / yolov4_cfg[net]
     model = YoloV5(config_path, ch=3, nc=num_classes)
     if pretrained:
-        checkpoint_url = model_urls[f"{net}_{_set_classes}"]
+        checkpoint_url = urlparse.urljoin(CHECKPOINT_STORAGE_URL, model_urls[f"{net}_{_set_classes}"])
         model = load_pretrained_weights(model, checkpoint_url, progress, device)
     return model.to(device)
 
@@ -57,10 +62,10 @@ def yolo4_6(
     net="yolov4s", _set_classes="voc_20", num_classes=20, pretrained=False,
     progress=True, device="cuda",
 ):
-    config_path = get_project_root() / yolov4_cfg[net]
+    config_path = get_project_root() / CFG_PATH / yolov4_cfg[net]
     model = YoloV5_6(config_path, ch=3, nc=num_classes)
     if pretrained:
-        checkpoint_url = model_urls[f"{net}_{_set_classes}"]
+        checkpoint_url = urlparse.urljoin(CHECKPOINT_STORAGE_URL, model_urls[f"{net}_{_set_classes}"])
         model = load_pretrained_weights(model, checkpoint_url, progress, device)
     return model.to(device)
 
