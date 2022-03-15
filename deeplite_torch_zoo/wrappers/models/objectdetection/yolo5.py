4f98107e0785d12650d695115e3ada406efe7de7
ivan-lazarevich <92095665+ivan-lazarevich@users.noreply.github.com>
2022-03-15 19:45:17 +0300

    Update model output tests (#157)

--- deeplite_torch_zoo/wrappers/models/objectdetection/yolo5.py
+++ deeplite_torch_zoo/wrappers/models/objectdetection/yolo5.py
@@ -25,33 +25,43 @@ CFG_PATH = "deeplite_torch_zoo/src/objectdetection/configs/model_configs"
 CHECKPOINT_STORAGE_URL = "http://download.deeplite.ai/zoo/models/"
 
 model_urls = {
+    "yolov5l_wider_face_8": "yolo5l-widerface-8cls-898_cdedd11381dbf565.pt",
+    "yolov5m_wider_face_8": "yolo5m-widerface-8cls-878_8a99aaf8b8b9157b.pt",
+    "yolov5_6s_coco_80": "yolov5_6s-coco-80classes_301-8ff1dabeec225366.pt",
+    "yolov5_6m_coco_80": "yolov5_6m-coco-80classes_374-f93fa94b629c45ab.pt",
+    "yolov5_6n_coco_80": "yolov5_6n-coco-80classes_211-e9e44a7de1f08ea2.pt",
+}
+
+voc_model_urls = {
     "yolov5s_voc_20": "yolov5s_voc-0_837-1e922891b803a8b7.pt",
     "yolov5m_voc_20": "yolo5m-voc-20classes_882-1d8265513714a3f6.pt",
     "yolov5l_voc_20": "yolo5l-voc-20classes_899-411aefb761eafaa3.pt",
     "yolov5x_voc_20": "yolo5x-voc-20classes_905-e8ddd018ae29751f.pt",
-    "yolov5l_wider_face_8": "yolo5l-widerface-8cls-898_cdedd11381dbf565.pt",
-    "yolov5m_wider_face_8": "yolo5m-widerface-8cls-878_8a99aaf8b8b9157b.pt",
     "yolov5l_voc_24": "yolo5l_voc-24_885_391dfc95d193faf5.pt",
     "yolov5m_voc_24": "yolo5m_voc_24_871_54be57d3f5a35a7b.pt",
-    "yolov5_6s_voc_20": "yolo5_6s-voc-20classes_871-4ceb1b22b227c05c.pt",
     "yolov5_6n_voc_20": "yolo5_6n-voc-20classes_762-a6b8573a32ebb4c8.pt",
+    "yolov5_6s_voc_20": "yolo5_6s-voc-20classes_871-4ceb1b22b227c05c.pt",
     "yolov5_6m_voc_20": "yolo5_6m-voc-20classes_902-50c151baffbf896e.pt",
     "yolov5_6l_voc_20": "yolov5_6l-voc-20classes_875_3fb90f0c405f170c.pt",
     "yolov5_6x_voc_20": "yolov5_6x-voc-20classes_884_a2b6fb7234218cf6.pt",
-    "yolov5_6s_relu_voc_20": "yolov5_6s_relu-voc-20classes-819_a35dff53b174e383.pt",
-    "yolov5_6m_relu_voc_20": "yolov5_6m_relu-voc-20classes-856_c5c23135e6d5012f.pt",
-    "yolov5_6s_coco_80": "yolov5_6s-coco-80classes_301-8ff1dabeec225366.pt",
-    "yolov5_6m_coco_80": "yolov5_6m-coco-80classes_374-f93fa94b629c45ab.pt",
-    "yolov5_6n_coco_80": "yolov5_6n-coco-80classes_211-e9e44a7de1f08ea2.pt",
-    "yolov5_6s_person_detection_1": "yolov5_6s-person-detection-1class_738-9e9ac9dae14b0dcd.pt",
-    "yolov5_6n_person_detection_1": "yolov5_6n-person-detection-1class_696-fff2a2c720e20752.pt",
-    "yolov5_6s_relu_person_detection_1": "yolov5_6s_relu-person-detection-1class_682-45ae979a06b80767.pt",
-    "yolov5_6m_relu_person_detection_1": "yolov5_6m_relu-person-detection-1class_709-3f59321c540d2d1c.pt",
-    "yolov5_6n_relu_person_detection_1": "yolov5_6n_relu-person-detection-1class_621-6794298f12d33ba8.pt",
     "yolov5_6n_voc07_20": "yolov5_6n-voc07-20classes-620_037230667eff7b12.pt",
     "yolov5_6s_voc07_20": "yolov5_6s-voc07-20classes-687_4d221fd4edc09ce1.pt",
+    "yolov5_6s_relu_voc_20": "yolov5_6s_relu-voc-20classes-819_a35dff53b174e383.pt",
+    "yolov5_6m_relu_voc_20": "yolov5_6m_relu-voc-20classes-856_c5c23135e6d5012f.pt",
+}
+
+person_detection_model_urls = {
+    "yolov5_6n_person_detection_1": "yolov5_6n-person-detection-1class_696-fff2a2c720e20752.pt",
+    "yolov5_6s_person_detection_1": "yolov5_6s-person-detection-1class_738-9e9ac9dae14b0dcd.pt",
+    "yolov5_6n_relu_person_detection_1": "yolov5_6n_relu-person-detection-1class_621-6794298f12d33ba8.pt",
+    "yolov5_6s_relu_person_detection_1": "yolov5_6s_relu-person-detection-1class_682-45ae979a06b80767.pt",
+    "yolov5_6m_relu_person_detection_1": "yolov5_6m_relu-person-detection-1class_709-3f59321c540d2d1c.pt",
     "yolov5_6sa_person_detection_1": "yolov5_6sa-person-detection-1class_659_015807ae6899af0f.pt",
 }
+
+model_urls.update(voc_model_urls)
+model_urls.update(person_detection_model_urls)
+
 
 yolov5_cfg = {
     "yolov5s": "yolov5s.yaml",
