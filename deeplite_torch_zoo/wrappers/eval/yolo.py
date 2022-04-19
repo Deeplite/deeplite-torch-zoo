5e4093ecbf7d28884ebb9ddc117d7016b018e1c2
ivan-lazarevich <92095665+ivan-lazarevich@users.noreply.github.com>
2022-04-19 06:47:21 +0300

    Align yolo train script with api (#172)

--- deeplite_torch_zoo/wrappers/eval/yolo.py
+++ deeplite_torch_zoo/wrappers/eval/yolo.py
@@ -1,27 +1,4 @@
-from functools import partial
-
-from deeplite_torch_zoo.src.objectdetection.eval.coco.coco_evaluator import yolo_eval_coco
-from deeplite_torch_zoo.src.objectdetection.eval.lisa_eval import yolo_eval_lisa
-from deeplite_torch_zoo.src.objectdetection.eval.wider_face_eval import yolo_eval_wider_face
-from deeplite_torch_zoo.src.objectdetection.eval.voc.voc_evaluator import yolo_eval_voc
-
-
-__all__ = ["get_eval_func", "yolo_eval_coco", "yolo_eval_voc",
-    "yolo_eval_lisa", "yolo_eval_wider_face"]
-
-
-def get_eval_func(dataset_name):
-    EVAL_FN_MAP = {
-        'voc07': partial(yolo_eval_voc, is_07_subset=True),
-        'voc': yolo_eval_voc,
-        'coco': yolo_eval_coco,
-        'lisa': yolo_eval_lisa,
-        'wider_face': yolo_eval_wider_face,
-        'person_detection': yolo_eval_voc,
-        'car_detection': partial(yolo_eval_coco, subsample_categories=['car']),
-        'person_pet_vehicle_detection': yolo_eval_voc,
-    }
-    for key, wrapper_fn in EVAL_FN_MAP.items():
-        if key in dataset_name:
-            return wrapper_fn
-    return None
+from deeplite_torch_zoo.src.objectdetection.eval.coco.coco_evaluator import yolo_eval_coco  # pylint: disable=unused-import
+from deeplite_torch_zoo.src.objectdetection.eval.lisa_eval import yolo_eval_lisa  # pylint: disable=unused-import
+from deeplite_torch_zoo.src.objectdetection.eval.wider_face_eval import yolo_eval_wider_face  # pylint: disable=unused-import
+from deeplite_torch_zoo.src.objectdetection.eval.voc.voc_evaluator import yolo_eval_voc  # pylint: disable=unused-import
--- training_scripts/object_detection/train_yolo.py
+++ training_scripts/object_detection/train_yolo.py
@@ -5,6 +5,8 @@ import os
 import random
 import sys
 import time
+import yaml
+import datetime
 from copy import deepcopy
 from pathlib import Path
 
@@ -12,7 +14,7 @@ import numpy as np
 import torch
 import torch.distributed as dist
 import torch.nn as nn
-import yaml
+from torch.utils.tensorboard import SummaryWriter
 from torch.cuda import amp
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.optim import Adam, SGD, lr_scheduler
@@ -26,21 +28,20 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-from utils.general import init_seeds, print_args, set_logging, one_cycle, colorstr, methods, strip_optimizer
+from utils.general import init_seeds, print_args, set_logging, one_cycle, \
+    colorstr, strip_optimizer, AverageMeter
 from utils.torch_utils import EarlyStopping, ModelEMA, de_parallel, select_device
-from utils.loggers import Loggers
-from utils.callbacks import Callbacks
 
-from deeplite_torch_zoo import get_data_splits_by_name, create_model
-from deeplite_torch_zoo.wrappers.eval import get_eval_func
+from deeplite_torch_zoo import get_data_splits_by_name, get_eval_function, create_model
 from deeplite_torch_zoo.src.objectdetection.yolov5.models.yolov5_loss import YoloV5Loss
+
 import deeplite_torch_zoo.src.objectdetection.yolov5.configs.hyps.hyp_config_default as hyp_cfg_scratch
 import deeplite_torch_zoo.src.objectdetection.yolov5.configs.hyps.hyp_config_finetune as hyp_cfg_finetune
 import deeplite_torch_zoo.src.objectdetection.yolov5.configs.hyps.hyp_config_lisa as hyp_cfg_lisa
 
 
 LOGGER = logging.getLogger(__name__)
-LOCAL_RANK = int(os.getenv('LOCAL_RANK', -1))  # https://pytxorch.org/docs/stable/elastic/run.html
+LOCAL_RANK = int(os.getenv('LOCAL_RANK', -1))  # https://pytorch.org/docs/stable/elastic/run.html
 RANK = int(os.getenv('RANK', -1))
 WORLD_SIZE = int(os.getenv('WORLD_SIZE', 1))
 
@@ -74,10 +75,13 @@ def get_hyperparameter_dict(dataset_type, hp_config=None):
     return hyp_config.TRAIN, hyp_config
 
 
-def train(opt, device, callbacks):
-    save_dir, epochs, batch_size, resume, noval, nosave, workers, freeze, = \
-        Path(opt.save_dir), opt.epochs, opt.batch_size, \
-        opt.resume, opt.noval, opt.nosave, opt.workers, opt.freeze
+def train(opt, device):
+    epochs, batch_size, noval, nosave, workers, freeze, = \
+        opt.epochs, opt.batch_size, opt.noval, opt.nosave, opt.workers, opt.freeze
+
+    d = datetime.datetime.now()
+    run_id = '{:%Y-%m-%d__%H-%M-%S}'.format(d)
+    save_dir = Path(opt.save_dir) / run_id
 
     # Directories
     w = save_dir / 'weights'  # weights dir
@@ -92,31 +96,30 @@ def train(opt, device, callbacks):
         yaml.safe_dump(hyp, f, sort_keys=False)
     with open(save_dir / 'opt.yaml', 'w') as f:
         yaml.safe_dump(vars(opt), f, sort_keys=False)
+    tb_writer = SummaryWriter(save_dir)
     opt.img_dir = Path(opt.img_dir)
-
-    # Loggers
-    if RANK in [-1, 0]:
-        loggers = Loggers(save_dir, None, opt, hyp, LOGGER)  # loggers instance
-        # Register actions
-        for k in methods(loggers):
-            callbacks.register_action(k, callback=getattr(loggers, k))
 
     # Config
     cuda = device.type != 'cpu'
     init_seeds(1 + RANK)
 
     # Dataloaders
-    train_img_size = hyp["train_img_size"] if not opt.train_img_res \
-        else opt.train_img_res
-    test_img_size = train_img_size if not opt.test_img_res else opt.test_img_res
+    dataset_kwargs = {}
+    if opt.train_img_res:
+        dataset_kwargs = {'train_img_size': opt.train_img_res}
     dataset_splits = get_data_splits_by_name(
         data_root=opt.img_dir,
         dataset_name=opt.dataset_type,
         model_name=opt.model_name,
         batch_size=batch_size,
         num_workers=workers,
-        img_size=train_img_size,
+        distributed=(cuda and RANK != -1),
+        **dataset_kwargs
     )
+    test_img_size = dataset_splits["test"].dataset._img_size
+    train_img_size = dataset_splits["train"].dataset._img_size
+    if opt.test_img_res:
+        test_img_size = opt.test_img_res
 
     train_loader = dataset_splits["train"]
     dataset = train_loader.dataset
@@ -197,11 +200,8 @@ def train(opt, device, callbacks):
 
     # Process 0
     if RANK in [-1, 0]:
-        if not resume:
-            # Anchors
-            model.half().float()  # pre-reduce anchor precision
-
-        callbacks.run('on_pretrain_routine_end')
+        # Anchors
+        model.half().float()  # pre-reduce anchor precision
 
     # DDP mode
     if cuda and RANK != -1:
@@ -216,7 +216,8 @@ def train(opt, device, callbacks):
     model.nc = nc  # attach number of classes to model
     model.hyp = hyp  # attach hyperparameters to model
 
-    eval_function = get_eval_func(opt.dataset_type)
+    eval_function = get_eval_function(dataset_name='_'.join((opt.dataset_type, str(nc))),
+        model_name=opt.model_name)
     criterion = YoloV5Loss(
         model=model,
         num_classes=nc,
@@ -228,10 +229,14 @@ def train(opt, device, callbacks):
     t0 = time.time()
     nw = max(round(hyp['warmup_epochs'] * nb), 1000)  # number of warmup iterations, max(3 epochs, 1k iterations)
     last_opt_step = -1
-    maps = np.zeros(nc)  # mAP per class
     scheduler.last_epoch = start_epoch - 1  # do not move
     scaler = amp.GradScaler(enabled=cuda)
     stopper = EarlyStopping(patience=opt.patience)
+
+    loss_giou_mean = AverageMeter()
+    loss_conf_mean = AverageMeter()
+    loss_cls_mean = AverageMeter()
+    loss_mean = AverageMeter()
 
     LOGGER.info(f'Image sizes {train_img_size} train, {test_img_size} val\n'
                 f'Using {train_loader.num_workers} dataloader workers\n'
@@ -249,14 +254,12 @@ def train(opt, device, callbacks):
             pbar = tqdm(pbar, total=nb)  # progress bar
         optimizer.zero_grad()
         for i, (imgs, targets, labels_length, _) in pbar:  # batch
-            paths = None
             ni = i + nb * epoch  # number integrated batches (since train start)
             imgs = imgs.to(device, non_blocking=True).float()
 
             # Warmup
             if ni <= nw:
                 xi = [0, nw]  # x interp
-                # compute_loss.gr = np.interp(ni, xi, [0.0, 1.0])  # iou loss ratio (obj_loss = 1.0 or iou)
                 accumulate = max(1, np.interp(ni, xi, [1, nbs / batch_size]).round())
                 for j, x in enumerate(optimizer.param_groups):
                     # bias lr falls from 0.1 to lr0, all other lrs rise from 0.0 to lr0
@@ -281,6 +284,12 @@ def train(opt, device, callbacks):
                 # Update running mean of tracked metrics
                 loss_items = torch.tensor([loss_giou, loss_conf, loss_cls]).to(device)
 
+                if RANK in (-1, 0):
+                    loss_giou_mean.update(loss_giou, imgs.size(0))
+                    loss_conf_mean.update(loss_conf, imgs.size(0))
+                    loss_cls_mean.update(loss_cls, imgs.size(0))
+                    loss_mean.update(loss, imgs.size(0))
+
                 if RANK != -1:
                     loss *= WORLD_SIZE  # gradient averaged between devices in DDP mode
 
@@ -302,16 +311,20 @@ def train(opt, device, callbacks):
                 mem = f'{torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0:.3g}G'  # (GB)
                 pbar.set_description(('%10s' * 2 + '%10.4g' * 5) % (
                     f'{epoch}/{epochs - 1}', mem, *mloss, targets.shape[0], imgs.shape[-1]))
-                callbacks.run('on_train_batch_end', ni, model, imgs, targets, paths, False, opt.sync_bn)
             # end batch
 
         # Scheduler
-        lr = [x['lr'] for x in optimizer.param_groups]  # for loggers
         scheduler.step()
 
         if RANK in [-1, 0]:
+            for idx, param_group in enumerate(optimizer.param_groups):
+                tb_writer.add_scalar(f'learning_rate/gr{idx}', param_group['lr'], epoch)
+            tb_writer.add_scalar('train/giou_loss', loss_giou_mean.avg, epoch)
+            tb_writer.add_scalar('train/conf_loss', loss_conf_mean.avg, epoch)
+            tb_writer.add_scalar('train/cls_loss', loss_cls_mean.avg, epoch)
+            tb_writer.add_scalar('train/loss', loss_mean.avg, epoch)
+
             # mAP
-            callbacks.run('on_train_epoch_end', epoch=epoch)
             ema.update_attr(model, include=['yaml', 'nc', 'hyp', 'names', 'stride', 'class_weights'])
             final_epoch = (epoch + 1 == epochs) or stopper.possible_stop
             if (not noval or final_epoch) and epoch % opt.eval_freq == 0:  # Calculate mAP
@@ -327,7 +340,7 @@ def train(opt, device, callbacks):
                         subsample_categories=["car"],
                     )
 
-                Aps = eval_function(
+                ap_dict = eval_function(
                     ema.ema,
                     test_set,
                     gt=gt,
@@ -337,14 +350,16 @@ def train(opt, device, callbacks):
                     img_size=test_img_size,
                     progressbar=True,
                 )
-                LOGGER.info(f'Eval metrics: {Aps}')
+                LOGGER.info(f'Eval metrics: {ap_dict}')
+                tb_writer.add_scalar('eval/mAP', ap_dict['mAP'], epoch)
+                for eval_key, eval_value in ap_dict.items():
+                    if eval_key != 'mAP':
+                        tb_writer.add_scalar(f'ap_per_class/{eval_key}', eval_value, epoch)
 
             # Update best mAP
-            fi = Aps['mAP']
+            fi = ap_dict['mAP']
             if fi > best_fitness:
                 best_fitness = fi
-            log_vals = list(mloss) + lr
-            callbacks.run('on_fit_epoch_end', log_vals, epoch, best_fitness, fi)
 
             # Save model
             if (not nosave) or final_epoch:  # if save
@@ -362,7 +377,6 @@ def train(opt, device, callbacks):
                 if (epoch > 0) and (opt.save_period > 0) and (epoch % opt.save_period == 0):
                     torch.save(ckpt, w / f'epoch{epoch}.pt')
                 del ckpt
-                callbacks.run('on_model_save', last, epoch, final_epoch, best_fitness, fi)
 
             # Stop Single-GPU
             if RANK == -1 and stopper(epoch=epoch, fitness=fi):
@@ -405,7 +419,6 @@ def train(opt, device, callbacks):
                     )
                     LOGGER.info(f'Eval metrics: {Aps}')
 
-        callbacks.run('on_train_end', last, best, False, epoch, None)
         LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}")
 
     torch.cuda.empty_cache()
@@ -468,13 +481,12 @@ def parse_opt(known=False):
         "--eval-freq",
         dest="eval_freq",
         type=int,
-        default=10,
+        default=5,
         help="Evaluation run frequency (in training epochs)",
     )
 
     parser.add_argument('--epochs', type=int, default=300)
     parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs')
-    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
     parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
     parser.add_argument('--noval', action='store_true', help='only validate final epoch')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
@@ -482,7 +494,6 @@ def parse_opt(known=False):
     parser.add_argument('--adam', action='store_true', help='use torch.optim.Adam() optimizer')
     parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
     parser.add_argument('--workers', type=int, default=8, help='maximum number of dataloader workers')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--linear-lr', action='store_true', help='linear LR')
     parser.add_argument('--label-smoothing', type=float, default=0.0, help='Label smoothing epsilon')
     parser.add_argument('--patience', type=int, default=100, help='EarlyStopping patience (epochs without improvement)')
@@ -494,32 +505,22 @@ def parse_opt(known=False):
     return opt
 
 
-def main(opt, callbacks=Callbacks()):
+def main(opt):
     # Checks
     set_logging(RANK)
     if RANK in [-1, 0]:
         print_args(FILE.stem, opt)
-
-    # Resume
-    if opt.resume:  # resume an interrupted run
-        ckpt = opt.resume
-        assert os.path.isfile(ckpt), 'ERROR: --resume checkpoint does not exist'
-        with open(Path(ckpt).parent.parent / 'opt.yaml', errors='ignore') as f:
-            opt = argparse.Namespace(**yaml.safe_load(f))  # replace
-        opt.cfg, opt.weights, opt.resume = '', ckpt, True  # reinstate
-        LOGGER.info(f'Resuming training from {ckpt}')
 
     # DDP mode
     device = select_device(opt.device, batch_size=opt.batch_size)
     if LOCAL_RANK != -1:
         assert torch.cuda.device_count() > LOCAL_RANK, 'insufficient CUDA devices for DDP command'
         assert opt.batch_size % WORLD_SIZE == 0, '--batch-size must be multiple of CUDA device count'
-        assert not opt.image_weights, '--image-weights argument is not compatible with DDP training'
         torch.cuda.set_device(LOCAL_RANK)
         device = torch.device('cuda', LOCAL_RANK)
         dist.init_process_group(backend="nccl" if dist.is_nccl_available() else "gloo")
 
-    train(opt, device, callbacks)
+    train(opt, device)
     if WORLD_SIZE > 1 and RANK == 0:
         LOGGER.info('Destroying process group... ')
         dist.destroy_process_group()
