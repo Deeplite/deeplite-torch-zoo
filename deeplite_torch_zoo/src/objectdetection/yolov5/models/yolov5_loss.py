1ba0cef1b1e5b02a2672dcd24275dccf53a021e6
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2022-04-19 11:58:39 -0400

    multi-gpu support

--- deeplite_torch_zoo/src/objectdetection/yolov5/models/yolov5_loss.py
+++ deeplite_torch_zoo/src/objectdetection/yolov5/models/yolov5_loss.py
@@ -4,6 +4,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+from utils.torch_utils import de_parallel
+
 import deeplite_torch_zoo.src.objectdetection.configs.hyps.hyp_config_default as hyp_cfg_default
 from deeplite_torch_zoo.src.objectdetection.yolov5.utils.general import xyxy2cxcywh
 
