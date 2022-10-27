77fdf035b8b33e0a13cf32a0b9476e48203bc8cc
Ravish Kumar <ravish@deeplite08.deeplite.local>
2022-10-27 02:10:36 -0400

    added tiny modelsl on custom person dataset

--- deeplite_torch_zoo/wrappers/models/objectdetection/yolo5.py
+++ deeplite_torch_zoo/wrappers/models/objectdetection/yolo5.py
@@ -48,6 +48,9 @@ person_detection_model_urls = {
     "yolo5_6s_relu_person_detection": "yolov5_6s_relu-person-detection-1class_682-45ae979a06b80767.pt",
     "yolo5_6m_relu_person_detection": "yolov5_6m_relu-person-detection-1class_709-3f59321c540d2d1c.pt",
     "yolo5_6sa_person_detection": "yolov5_6sa-person-detection-1class_659_015807ae6899af0f.pt",
+    "yolov5_6n_relu_custom_person_detection": "yolov5_6n_relu-person-detection-1class_889.pt",
+    "tiny_yolov5_6n_relu_custom_person_detection": "yolov5_6n_tiny_relu-person-detection-1class_875.pt",
+    "tiny_yolov5_6n_hswish_custom_person_detection": "yolov5_6n_tiny_hs-person-detection-1class_891.pt",
 }
 
 model_urls.update(voc_model_urls)
@@ -62,8 +65,8 @@ yolov5_cfg = {
     "yolo5_6n": "yolov5_6n.yaml",
     "yolo5_6sa": "yolov5_6sa.yaml",
     "yolo5_6ma": "yolov5_6ma.yaml",
+    "tiny_yolov5_6n": "tiny_yolov5_6n.yaml"
 }
-
 
 MODEL_NAME_SUFFICES = ('relu', 'hswish')
 
@@ -88,6 +91,8 @@ MODEL_TAG_TO_WRAPPER_FN_MAP = {
     "^yolo5_6[nsmlx]a$": yolo5_6,
     "^yolo5_6[nsmlx]_relu$": partial(yolo5_6, activation_type="relu"),
     "^yolo5_6[nsmlx]_hswish$": partial(yolo5_6, activation_type="hardswish"),
+    "tiny_yolov5_6n_relu": partial(yolo5_6, activation_type="relu"),
+    "tiny_yolov5_6n_hswish": partial(yolo5_6, activation_type="hardswish"),
 }
 
 def make_wrapper_func(wrapper_name, model_name, dataset_name, num_classes):
@@ -115,6 +120,8 @@ ModelSet = namedtuple('ModelSet', ['num_classes', 'model_list'])
 wrapper_funcs = {
     'person_detection': ModelSet(1, ['yolo5_6n', 'yolo5_6s',
         'yolo5_6n_relu', 'yolo5_6s_relu', 'yolo5_6m_relu', 'yolo5_6sa']),
+    'custom_person_detection': ModelSet(1, ['yolov5_6n_relu', 'tiny_yolov5_6n_relu', 
+        'tiny_yolov5_6n_hswish']),
     'voc': ModelSet(20, ['yolo5_6n', 'yolo5_6s', 'yolo5_6m', 'yolo5_6l', 'yolo5_6x',
         'yolo5_6m_relu', 'yolo5_6s_relu', 'yolo5_6n_relu', 'yolo5_6n_hswish', 'yolo5_6s_hswish']),
     'coco': ModelSet(80, ['yolo5_6n', 'yolo5_6s', 'yolo5_6m', 'yolo5_6sa', 'yolo5_6ma',
