befb9e3a54dfa5f988f342573519b573277c1e76
Ravish Kumar <ravish@deeplite07.deeplite.local>
2022-11-15 10:49:20 -0500

    mobilent_96px addition

--- deeplite_torch_zoo/src/classification/mobilenets/mobilenetv1.py
+++ deeplite_torch_zoo/src/classification/mobilenets/mobilenetv1.py
@@ -47,3 +47,51 @@ class MobileNetV1(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
+
+class MobileNetV1_96(nn.Module):
+    def __init__(self, num_classes=2, width_mult=1.):
+        super(MobileNetV1_96, self).__init__()
+
+        def conv_bn(inp, oup, stride):
+            oup = int(oup*width_mult)
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+            )
+
+        def conv_dw(inp, oup, stride):
+            inp, oup = int(inp*width_mult), int(oup*width_mult)
+            return nn.Sequential(
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+            )
+
+        self.model = nn.Sequential(
+            conv_bn(3, 32, 2),
+            conv_dw(32, 64, 1),
+            conv_dw(64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 1024, 2),
+            conv_dw(1024, 1024, 1),
+            nn.AvgPool2d(3),
+        )
+        self.fc = nn.Linear(int(1024*width_mult), num_classes)
+
+    def forward(self, x):
+        x = self.model(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
--- deeplite_torch_zoo/wrappers/models/classification/vww/mobilenetv1.py
+++ deeplite_torch_zoo/wrappers/models/classification/vww/mobilenetv1.py
@@ -1,15 +1,15 @@
 from deeplite_torch_zoo.wrappers.models.utils import load_pretrained_weights
-from deeplite_torch_zoo.src.classification.mobilenets.mobilenetv1 import MobileNetV1
+from deeplite_torch_zoo.src.classification.mobilenets.mobilenetv1 import MobileNetV1, MobileNetV1_96
 from deeplite_torch_zoo.wrappers.registries import MODEL_WRAPPER_REGISTRY
 
 
-__all__ = ["mobilenet_v1_vww"]
+__all__ = ["mobilenet_v1_vww", 'mobilenet_v1_025_vww', 'mobilenet_v1_025_96px_vww']
 
 model_urls = {
     "mobilenetv1": "http://download.deeplite.ai/zoo/models/mobilenetv1-vww-84f65dc4bc649cd6.pth",
-    "mobilenetv1_0.25": "http://download.deeplite.ai/zoo/models/mobilenet_v1_0.25.pt"
+    "mobilenetv1_0.25": "http://download.deeplite.ai/zoo/models/mobilenet_v1_0.25.pt",
+    "mobilenetv1_0.25_96px": "http://download.deeplite.ai/zoo/models/mobilenet_v1_0.25_96px_798-8df2181bdab1433e.pt",
 }
-
 
 def _mobilenetv1_vww(arch, pretrained=False, progress=True, num_classes=2, device='cuda', width_mult=1.):
     model = MobileNetV1(num_classes=num_classes, width_mult=width_mult)
@@ -19,6 +19,13 @@ def _mobilenetv1_vww(arch, pretrained=False, progress=True, num_classes=2, devic
         model = load_pretrained_weights(model, checkpoint_url, progress, device)
     return model.to(device)
 
+def _mobilenetv1_vww_96(arch, pretrained=False, progress=True, num_classes=2, device='cuda', width_mult=0.25):
+    model = MobileNetV1_96(num_classes=num_classes, width_mult=width_mult)
+
+    if pretrained:
+        checkpoint_url = model_urls[arch]
+        model = load_pretrained_weights(model, checkpoint_url, progress, device)
+    return model.to(device)
 
 @MODEL_WRAPPER_REGISTRY.register(model_name='mobilenet_v1', dataset_name='vww', task_type='classification')
 def mobilenet_v1_vww(pretrained=False, progress=True, num_classes=2, device='cuda'):
@@ -27,3 +34,7 @@ def mobilenet_v1_vww(pretrained=False, progress=True, num_classes=2, device='cud
 @MODEL_WRAPPER_REGISTRY.register(model_name='mobilenet_v1_0.25', dataset_name='vww', task_type='classification')
 def mobilenet_v1_025_vww(pretrained=False, progress=True, num_classes=2, device='cuda',width_mult=0.25):
     return _mobilenetv1_vww("mobilenetv1_0.25", pretrained=pretrained, progress=progress, num_classes=num_classes, device=device, width_mult=width_mult)
+
+@MODEL_WRAPPER_REGISTRY.register(model_name='mobilenet_v1_0.25_96px', dataset_name='vww', task_type='classification')
+def mobilenet_v1_025_96px_vww(pretrained=False, progress=True, num_classes=2, device='cuda',width_mult=0.25):
+    return _mobilenetv1_vww_96("mobilenetv1_0.25_96px", pretrained=pretrained, progress=progress, num_classes=num_classes, device=device, width_mult=width_mult)
