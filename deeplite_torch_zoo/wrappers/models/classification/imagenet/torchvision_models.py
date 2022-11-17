185abfac046e9ba1dd14b18023fead7bc8ffa9f4
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2022-11-16 22:59:43 -0500

    add ultralytics classification train script

--- deeplite_torch_zoo/wrappers/models/classification/imagenet/torchvision_models.py
+++ deeplite_torch_zoo/wrappers/models/classification/imagenet/torchvision_models.py
@@ -49,6 +49,8 @@ tv_models = set(MODEL_NAMES) - set(timm.list_models())
 
 def make_wrapper_func(wrapper_fn_name, model_name_key):
     @MODEL_WRAPPER_REGISTRY.register(model_name=model_name_key, dataset_name='imagenet', task_type='classification')
+    @MODEL_WRAPPER_REGISTRY.register(model_name=model_name_key, dataset_name='food101',
+        task_type='classification', has_checkpoint=False)
     def wrapper_func(pretrained=False, progress=True, device="cuda", num_classes=1000):
         model = torchvision.models.__dict__[model_name_key](pretrained=pretrained, num_classes=num_classes)
         return model.to(device)
