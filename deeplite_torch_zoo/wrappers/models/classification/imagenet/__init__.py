1fb353cb4ad0ac58f08371eb2bceb0dec4cf2bea
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2022-11-19 20:56:52 -0500

    add pytorchcv models

--- deeplite_torch_zoo/wrappers/models/classification/imagenet/__init__.py
+++
@@ -1,3 +0,0 @@
-from .torchvision_models import *
-from .torchvision_models_quantized import *
-from .timm_models import *
--- deeplite_torch_zoo/wrappers/models/classification/imagenet/pytorchcv_models.py
+++ deeplite_torch_zoo/wrappers/models/classification/imagenet/pytorchcv_models.py
@@ -1,8 +1,8 @@
 import timm
-import torchvision
 from deeplite_torch_zoo.wrappers.models.classification.imagenet.impl_model_names import \
     IMPL_MODEL_NAMES
 from deeplite_torch_zoo.wrappers.registries import MODEL_WRAPPER_REGISTRY
+from pytorchcv.model_provider import get_model as ptcv_get_model
 
 TIMM_MODELS = timm.list_models()
 
@@ -12,17 +12,17 @@ def make_wrapper_func(wrapper_fn_name, register_model_name_key, model_name_key):
     @MODEL_WRAPPER_REGISTRY.register(model_name=register_model_name_key, dataset_name='food101',
         task_type='classification', has_checkpoint=False)
     def wrapper_func(pretrained=False, progress=True, device="cuda", num_classes=1000):
-        model = torchvision.models.__dict__[model_name_key](pretrained=pretrained, num_classes=num_classes)
+        model = ptcv_get_model(model_name_key, pretrained=pretrained, num_classes=num_classes)
         return model.to(device)
 
     wrapper_func.__name__ = wrapper_fn_name
     return wrapper_func
 
 
-for model_name_tag in IMPL_MODEL_NAMES['torchvision']:
+for model_name_tag in IMPL_MODEL_NAMES['pytorchcv']:
     register_model_name_tag = model_name_tag
     if model_name_tag in TIMM_MODELS:
-        register_model_name_tag = "_".join((model_name_tag, "torchvision"))
+        register_model_name_tag = "_".join((model_name_tag, "pytorchcv"))
     wrapper_name = "_".join((model_name_tag, "imagenet"))
     globals()[wrapper_name] = make_wrapper_func(wrapper_name,
         register_model_name_key=register_model_name_tag, model_name_key=model_name_tag)
