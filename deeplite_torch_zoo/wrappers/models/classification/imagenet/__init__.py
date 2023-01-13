f7a60024506a4dd4451cd2062a6c85dc3389bdb8
shahrukh-dl <97095923+shahrukh-dl@users.noreply.github.com>
2023-01-14 03:29:05 +0530

    Add MobileOne models (#243)

--- deeplite_torch_zoo/wrappers/models/classification/imagenet/__init__.py
+++ deeplite_torch_zoo/wrappers/models/classification/imagenet/__init__.py
@@ -3,3 +3,4 @@ from .torchvision_models import *  # pylint: disable=unused-import
 from .torchvision_models_quantized import *  # pylint: disable=unused-import
 from .pytorchcv_models import *  # pylint: disable=unused-import
 from .zoo_models import *  # pylint: disable=unused-import
+from .mobileone import *  # pylint: disable=unused-import
