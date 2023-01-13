721453420cd6673f927f6db96d3b021da994df85
honnesh-deeplite <86160584+honnesh-deeplite@users.noreply.github.com>
2023-01-13 00:49:16 -0500

    Update __init__.py

--- deeplite_torch_zoo/wrappers/datasets/classification/__init__.py
+++ deeplite_torch_zoo/wrappers/datasets/classification/__init__.py
@@ -2,10 +2,6 @@ try:
     from .vww import *
 except ImportError:
     pass
-try:
-    from .imagenette import *
-except ImportError:
-    pass
 from .cifar import *
 from .imagenet import *
 from .mnist import *
@@ -13,4 +9,4 @@ from .vww import *
 from .tiny_imagenet import *
 from .food101 import *
 from .flowers102 import *
-
+from .imagenette import *
