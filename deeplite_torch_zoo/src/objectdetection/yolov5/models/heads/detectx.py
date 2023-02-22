fix yolox loss computation

deeplite_torch_zoo/src/objectdetection/yolov5/models/heads/detectx.py
@@ -2,28 +2,63 @@
 YOLOX-specific modules
 """
 
-import sys
-from pathlib import Path
-import logging
+import math
 
 import torch
-
-FILE = Path(__file__).absolute()
-sys.path.append(FILE.parents[1].as_posix())
-
-from deeplite_torch_zoo.src.objectdetection.yolov5.models.common import *
-from deeplite_torch_zoo.src.objectdetection.yolov5.models.experimental import *
-
-from deeplite_torch_zoo.src.objectdetection.yolov5.models.losses.yolox_loss import *
 import torch.nn.functional as F
 from torch.cuda.amp import autocast
 
-try:
-    import thop
-except ImportError:
-    thop = None
+from deeplite_torch_zoo.src.objectdetection.yolov5.models.common import *
+from deeplite_torch_zoo.src.objectdetection.yolov5.models.experimental import *
+from deeplite_torch_zoo.src.objectdetection.yolov5.models.losses.yolox_loss import *
 
-LOGGER = logging.getLogger(__name__)
+
+class IOUloss(nn.Module):
+    def __init__(self, reduction="none", loss_type="iou"):
+        super(IOUloss, self).__init__()
+        self.reduction = reduction
+        self.loss_type = loss_type
+
+    def forward(self, pred, target, xyxy=False):
+        assert pred.shape[0] == target.shape[0]
+
+        pred = pred.view(-1, 4)
+        target = target.view(-1, 4)
+        if xyxy:
+            tl = torch.max(pred[:, :2], target[:, :2])
+            br = torch.min(pred[:, 2:], target[:, 2:])
+            area_p = torch.prod(pred[:, 2:] - pred[:, :2], 1)
+            area_g = torch.prod(target[:, 2:] - target[:, :2], 1)
+        else:
+            tl = torch.max((pred[:, :2] - pred[:, 2:] / 2), (target[:, :2] - target[:, 2:] / 2))
+            br = torch.min((pred[:, :2] + pred[:, 2:] / 2), (target[:, :2] + target[:, 2:] / 2))
+            area_p = torch.prod(pred[:, 2:], 1)
+            area_g = torch.prod(target[:, 2:], 1)
+
+        hw = (br - tl).clamp(min=0)  # [rows, 2]
+        area_i = torch.prod(hw, 1)
+
+        iou = (area_i) / (area_p + area_g - area_i + 1e-16)
+
+        if self.loss_type == "iou":
+            loss = 1 - iou ** 2
+        elif self.loss_type == "giou":
+            if xyxy:
+                c_tl = torch.min(pred[:, :2], target[:, :2])
+                c_br = torch.max(pred[:, 2:], target[:, 2:])
+            else:
+                c_tl = torch.min((pred[:, :2] - pred[:, 2:] / 2), (target[:, :2] - target[:, 2:] / 2))
+                c_br = torch.max((pred[:, :2] + pred[:, 2:] / 2), (target[:, :2] + target[:, 2:] / 2))
+            area_c = torch.prod(c_br - c_tl, 1)
+            giou = iou - (area_c - area_i) / area_c.clamp(1e-16)
+            loss = 1 - giou.clamp(min=-1.0, max=1.0)
+
+        if self.reduction == "mean":
+            loss = loss.mean()
+        elif self.reduction == "sum":
+            loss = loss.sum()
+
+        return loss
 
 
 class DetectX(nn.Module):
@@ -31,7 +66,8 @@ class DetectX(nn.Module):
     onnx_dynamic = False  # ONNX export parameter
     export = False
 
-    def __init__(self, num_classes, anchors=1, in_channels=(128, 128, 128, 128, 128, 128), inplace=True, prior_prob=1e-2,):
+    def __init__(self, num_classes, anchors=1, in_channels=(128, 128, 128, 128, 128, 128),
+        inplace=True, prior_prob=1e-2,):
         super().__init__()
         if isinstance(anchors, (list, tuple)):
             self.n_anchors = len(anchors)
@@ -161,7 +197,6 @@ class DetectX(nn.Module):
             return outputs
 
     def forward(self, x):
-        print(len(x))
         outputs = self._forward(x)
 
         if self.training:
@@ -177,7 +212,7 @@ class DetectX(nn.Module):
                 bs, no = outputs[0].shape[0], outputs[0].shape[1]
 
             self.hw = [out.shape[-2:] for out in outputs]
-            
+
             # [batch, n_anchors_all, 85]
             outs = []
             for i in range(len(outputs)):
@@ -256,8 +291,8 @@ class DetectX(nn.Module):
         out = torch.cat([xy, wh, score], -1)
         return out
 
-    def get_losses(self, bbox_preds, cls_preds, obj_preds, origin_preds, org_xy_shifts, xy_shifts, expanded_strides,
-                   center_ltrbes, whwh, labels, dtype,):
+    def get_losses(self, bbox_preds, cls_preds, obj_preds, origin_preds, org_xy_shifts,
+        xy_shifts, expanded_strides, center_ltrbes, whwh, labels, dtype):
         # calculate targets
         nlabel = labels[:,0].long().bincount(minlength=cls_preds.shape[0]).tolist()
         batch_gt_classes = labels[:,1].type_as(cls_preds).contiguous()  # [num_gt, 1]
@@ -292,7 +327,8 @@ class DetectX(nn.Module):
                 _num_gts = num_gts + num_gt
                 org_gt_bboxes_per_image = batch_org_gt_bboxes[num_gts:_num_gts]
                 gt_bboxes_per_image = batch_gt_bboxes[num_gts:_num_gts]
-                gt_classes = batch_gt_classes[:,num_gts:_num_gts]
+                # gt_classes = batch_gt_classes[:,num_gts:_num_gts]
+                gt_classes = batch_gt_classes[num_gts:_num_gts]
                 num_gts = _num_gts
                 bboxes_preds_per_image = bbox_preds[batch_idx]
                 cls_preds_per_image = cls_preds[batch_idx]
@@ -319,7 +355,7 @@ class DetectX(nn.Module):
                         xy_shifts,
                     )
                 except RuntimeError:
-                    LOGGER.error(
+                    print(
                         "OOM RuntimeError is raised due to the huge memory cost during label assignment. \
                            CPU mode is applied in this batch. If you want to avoid this issue, \
                            try to reduce the batch size or image size."

deeplite_torch_zoo/src/objectdetection/yolov5/models/losses/loss_utils.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -33,7 +35,7 @@ def get_yolov5_targets(raw_targets, labels_length, img_size, device):
         prv_index = cum_index[i]
 
     targets[:, 2:6] /= img_size  # Normalize to 0 - 1
-    return
+    return targets
 
 
 class FocalLoss(nn.Module):

deeplite_torch_zoo/src/objectdetection/yolov5/models/losses/yolov5_loss.py
@@ -104,9 +104,7 @@ class YoloV5Loss(nn.Module):
         loss = lbox + lobj + lcls
         return (
             loss * bs,
-            lbox,
-            lobj,
-            lcls,
+            torch.tensor([lbox, lobj, lcls]).to(self.device)
         )
 
     def build_targets(self, p, targets):

deeplite_torch_zoo/src/objectdetection/yolov5/models/losses/yolox_loss.py
@@ -1,57 +1,8 @@
 import torch
-import torch.nn as nn
 
-def is_parallel(model):
-    # Returns True if model is of type DP or DDP
-    return type(model) in (nn.parallel.DataParallel, nn.parallel.DistributedDataParallel)
+from deeplite_torch_zoo.src.objectdetection.yolov5.models.losses.loss_utils import (
+    get_yolov5_targets, is_parallel)
 
-
-class IOUloss(nn.Module):
-    def __init__(self, reduction="none", loss_type="iou"):
-        super(IOUloss, self).__init__()
-        self.reduction = reduction
-        self.loss_type = loss_type
-
-    def forward(self, pred, target, xyxy=False):
-        assert pred.shape[0] == target.shape[0]
-
-        pred = pred.view(-1, 4)
-        target = target.view(-1, 4)
-        if xyxy:
-            tl = torch.max(pred[:, :2], target[:, :2])
-            br = torch.min(pred[:, 2:], target[:, 2:])
-            area_p = torch.prod(pred[:, 2:] - pred[:, :2], 1)
-            area_g = torch.prod(target[:, 2:] - target[:, :2], 1)
-        else:
-            tl = torch.max((pred[:, :2] - pred[:, 2:] / 2), (target[:, :2] - target[:, 2:] / 2))
-            br = torch.min((pred[:, :2] + pred[:, 2:] / 2), (target[:, :2] + target[:, 2:] / 2))
-            area_p = torch.prod(pred[:, 2:], 1)
-            area_g = torch.prod(target[:, 2:], 1)
-
-        hw = (br - tl).clamp(min=0)  # [rows, 2]
-        area_i = torch.prod(hw, 1)
-
-        iou = (area_i) / (area_p + area_g - area_i + 1e-16)
-
-        if self.loss_type == "iou":
-            loss = 1 - iou ** 2
-        elif self.loss_type == "giou":
-            if xyxy:
-                c_tl = torch.min(pred[:, :2], target[:, :2])
-                c_br = torch.max(pred[:, 2:], target[:, 2:])
-            else:
-                c_tl = torch.min((pred[:, :2] - pred[:, 2:] / 2), (target[:, :2] - target[:, 2:] / 2))
-                c_br = torch.max((pred[:, :2] + pred[:, 2:] / 2), (target[:, :2] + target[:, 2:] / 2))
-            area_c = torch.prod(c_br - c_tl, 1)
-            giou = iou - (area_c - area_i) / area_c.clamp(1e-16)
-            loss = 1 - giou.clamp(min=-1.0, max=1.0)
-
-        if self.reduction == "mean":
-            loss = loss.mean()
-        elif self.reduction == "sum":
-            loss = loss.sum()
-
-        return loss
 
 class ComputeXLoss:
     # Compute losses
@@ -62,10 +13,16 @@ class ComputeXLoss:
         self.det = det
         self.device = device
 
-    def __call__(self, p, targets):  # predictions, targets, model
-        #device = targets.device
-        if (not self.det.training) or (len(p) == 0):
-            return torch.zeros(1, device=self.device), torch.zeros(4, device=self.device)
+    def __call__(self, p, raw_targets, labels_length, img_size):  # predictions, targets, model
+        targets = get_yolov5_targets(raw_targets, labels_length, img_size, self.device)
 
-        (loss, iou_loss, obj_loss, cls_loss, l1_loss, num_fg,) = self.det.get_losses(*p, targets, dtype=p[0].dtype, )
+        if (not self.det.training) or (len(p) == 0) or (len(targets) == 0):
+            return torch.tensor(0.0, device=self.device, requires_grad=True), \
+                torch.zeros(4, device=self.device, requires_grad=True)
+
+        loss, iou_loss, obj_loss, cls_loss, l1_loss, _ = self.det.get_losses(
+            *p,
+            targets,
+            dtype=p[0].dtype)
+
         return loss, torch.hstack((iou_loss, obj_loss, cls_loss, l1_loss)).detach()

training_scripts/classification/ultralytics/train.py
@@ -11,18 +11,18 @@ import torch
 import torch.distributed as dist
 import torch.nn.functional as F
 import torch.optim.lr_scheduler as lr_scheduler
-from deeplite_torch_zoo import (create_model, get_data_splits_by_name,
-                                get_eval_function)
+from kd import KDTeacher
 from torch.cuda import amp
 from tqdm import tqdm
-
-from kd import KDTeacher
 from utils.general import (LOGGER, WorkingDirectory, colorstr, increment_path,
                            init_seeds, print_args, yaml_save)
 from utils.torch_utils import (GenericLogger, ModelEMA, select_device,
                                smart_DDP, smart_optimizer,
                                smartCrossEntropyLoss,
                                torch_distributed_zero_first)
+
+from deeplite_torch_zoo import (create_model, get_data_splits_by_name,
+                                get_eval_function)
 
 ROOT = Path.cwd()
 
@@ -61,7 +61,7 @@ def train(opt, device):
     trainloader, testloader = dataloaders['train'], dataloaders['test']
 
     # Model
-    opt.num_classes = 2 #len(trainloader.dataset.classes)
+    opt.num_classes = len(trainloader.dataset.classes)
     with torch_distributed_zero_first(LOCAL_RANK), WorkingDirectory(ROOT):
         model = create_model(
             model_name=opt.model,
@@ -82,7 +82,7 @@ def train(opt, device):
     # Eval function
     evaluation_fn = get_eval_function(
         model_name=opt.model,
-        dataset_name=opt.dataset,
+        dataset_name=opt.pretraining_dataset,
     )
 
     # Info

training_scripts/object_detection/train_yolo.py
@@ -264,12 +264,11 @@ def train(opt, device):
     for epoch in range(start_epoch, epochs):  # epoch
         model.train()
 
-        if opt.model_name.split('_')[0] == 'yolox':
-            mloss = torch.zeros(4, device=device)  # mean losses
-            LOGGER.info(('\n' + '%10s' * 8) % ('Epoch', 'gpu_mem', 'box', 'obj', 'cls', 'l1', 'labels', 'img_size'))
-        else:
+        if not 'yolox' in opt.model_name:
             mloss = torch.zeros(3, device=device)  # mean losses
-            LOGGER.info(('\n' + '%10s' * 7) % ('Epoch', 'gpu_mem', 'box', 'obj', 'cls', 'labels', 'img_size'))
+        else:
+            mloss = torch.zeros(4, device=device)  # mean losses
+        LOGGER.info(('\n' + '%10s' * 7) % ('Epoch', 'gpu_mem', 'box', 'obj', 'cls', 'labels', 'img_size'))
 
         if RANK != -1:
             train_loader.sampler.set_epoch(epoch)
@@ -303,19 +302,14 @@ def train(opt, device):
             with amp.autocast(enabled=cuda):
                 pred = model(imgs)  # forward
 
-                if opt.model_name.split('_')[0] == 'yolox':
-                    loss, loss_items = criterion(pred, targets.to(device))
-                else:
-                    loss, loss_giou, loss_conf, loss_cls = criterion(
-                        pred, targets, labels_length, imgs.shape[-1]
-                    )
-                    # Update running mean of tracked metrics
-                    loss_items = torch.tensor([loss_giou, loss_conf, loss_cls]).to(device)
+                loss, loss_items = criterion(
+                    pred, targets, labels_length, imgs.shape[-1]
+                )
 
                 if RANK in (-1, 0):
-                    loss_giou_mean.update(loss_giou, imgs.size(0))
-                    loss_conf_mean.update(loss_conf, imgs.size(0))
-                    loss_cls_mean.update(loss_cls, imgs.size(0))
+                    loss_giou_mean.update(loss_items[0], imgs.size(0))
+                    loss_conf_mean.update(loss_items[1], imgs.size(0))
+                    loss_cls_mean.update(loss_items[2], imgs.size(0))
                     loss_mean.update(loss, imgs.size(0))
 
                 if RANK != -1:
@@ -338,7 +332,7 @@ def train(opt, device):
                 mloss = (mloss * i + loss_items) / (i + 1)  # update mean losses
                 mem = f'{torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0:.3g}G'  # (GB)
                 pbar.set_description(('%10s' * 2 + '%10.4g' * 5) % (
-                    f'{epoch}/{epochs - 1}', mem, *mloss, targets.shape[0], imgs.shape[-1]))
+                    f'{epoch}/{epochs - 1}', mem, *mloss[:3], targets.shape[0], imgs.shape[-1]))
             # end batch
 
         # Scheduler
@@ -460,7 +454,7 @@ def parse_opt(known=False):
         "--eval-skip-epochs",
         dest="eval_skip_epochs",
         type=int,
-        default=1,
+        default=100,
         help="Skip evaluation for this number of epochs in the beginning",
     )
 