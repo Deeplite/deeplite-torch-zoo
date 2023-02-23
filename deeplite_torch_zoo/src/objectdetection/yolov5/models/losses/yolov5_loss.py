9cd7aa9ebfbb85f0f4238de5c0de0403da9b4a7a
lzrwch <92095665+lzrwch@users.noreply.github.com>
2023-02-23 17:31:35 -0500

    NT compatibility updates  (#271)

--- deeplite_torch_zoo/src/objectdetection/yolov5/models/losses/yolov5_loss.py
+++ deeplite_torch_zoo/src/objectdetection/yolov5/models/losses/yolov5_loss.py
@@ -104,7 +104,7 @@ class YoloV5Loss(nn.Module):
         loss = lbox + lobj + lcls
         return (
             loss * bs,
-            torch.tensor([lbox, lobj, lcls]).to(self.device)
+            torch.tensor([lbox, lobj, lcls], requires_grad=True).to(self.device)
         )
 
     def build_targets(self, p, targets):
