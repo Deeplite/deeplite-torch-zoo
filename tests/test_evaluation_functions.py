7996b1bb2e0b38d9dc81b3295b9137a02e67c5ed
lzrwch <92095665+lzrwch@users.noreply.github.com>
2023-03-15 23:44:11 -0400

    Print table in eval function tests (#282)

--- tests/test_evaluation_functions.py
+++ tests/test_evaluation_functions.py
@@ -1,4 +1,4 @@
-from deeplite_torch_zoo import (get_eval_function, list_models)
+from deeplite_torch_zoo import get_eval_function, list_models
 from deeplite_torch_zoo.src.segmentation.deeplab.eval import evaluate_deeplab
 from deeplite_torch_zoo.src.segmentation.fcn.eval import evaluate_fcn
 from deeplite_torch_zoo.src.segmentation.Unet.eval import (eval_net,
@@ -25,7 +25,7 @@ segmentation_eval_list = (evaluate_fcn, eval_net, eval_net_miou, evaluate_deepla
 
 def test_eval_classification():
     all_classification_models = list_models(
-        task_type_filter="classification", print_table=False, return_list=True
+        task_type_filter="classification", print_table=True, return_list=True
     )
 
     for (model_name, dataset_name) in all_classification_models:
@@ -36,7 +36,7 @@ def test_eval_classification():
 def test_eval_segmentation():
     all_segmentation_models = list_models(
         task_type_filter="semantic_segmentation",
-        print_table=False,
+        print_table=True,
         return_list=True,
     )
     for (model_name, dataset_name) in all_segmentation_models:
@@ -46,7 +46,7 @@ def test_eval_segmentation():
 
 def test_eval_objectdetection():
     all_od_models = list_models(
-        task_type_filter="object_detection", print_table=False, return_list=True
+        task_type_filter="object_detection", print_table=True, return_list=True
     )
     for (model_name, dataset_name) in all_od_models:
         eval_fn = get_eval_function(model_name=model_name, dataset_name=dataset_name)
