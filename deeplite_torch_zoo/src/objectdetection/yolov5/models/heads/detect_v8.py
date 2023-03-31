a37d9acddf4b91ed2562b010295efd304ea454f5
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2023-03-31 02:19:19 -0400

    v8 head activation

--- deeplite_torch_zoo/src/objectdetection/yolov5/models/heads/detect_v8.py
+++ deeplite_torch_zoo/src/objectdetection/yolov5/models/heads/detect_v8.py
@@ -60,7 +60,7 @@ class DetectV8(nn.Module):
     anchors = torch.empty(0)  # init
     strides = torch.empty(0)  # init
 
-    def __init__(self, nc=80, ch=()):  # detection layer
+    def __init__(self, nc=80, ch=(), act='relu'):  # detection layer
         super().__init__()
         self.nc = nc  # number of classes
         self.nl = len(ch)  # number of detection layers
@@ -70,8 +70,8 @@ class DetectV8(nn.Module):
 
         c2, c3 = max((16, ch[0] // 4, self.reg_max * 4)), max(ch[0], self.nc)  # channels
         self.cv2 = nn.ModuleList(
-            nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch)
-        self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
+            nn.Sequential(Conv(x, c2, 3, act=act), Conv(c2, c2, 3, act=act), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch)
+        self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, c3, 3, act=act), Conv(c3, c3, 3, act=act), nn.Conv2d(c3, self.nc, 1)) for x in ch)
         self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
 
     def forward(self, x):
