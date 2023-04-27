fcdf32b8b300e5baa6b7766e7c943c665d24505f
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2023-04-27 00:31:28 -0400

    add base YOLOv6-3.0

--- deeplite_torch_zoo/src/objectdetection/flexible_yolo/model.py
+++ deeplite_torch_zoo/src/objectdetection/flexible_yolo/model.py
@@ -9,6 +9,10 @@ from deeplite_torch_zoo.src.objectdetection.flexible_yolo.backbone import \
     build_backbone
 from deeplite_torch_zoo.src.objectdetection.flexible_yolo.neck import \
     build_neck
+from deeplite_torch_zoo.src.objectdetection.flexible_yolo.yolov6 import \
+    build_network
+from deeplite_torch_zoo.src.objectdetection.flexible_yolo.yolov6.config import \
+    Config
 from deeplite_torch_zoo.src.objectdetection.yolov5.models.heads.detect import \
     Detect
 from deeplite_torch_zoo.src.objectdetection.yolov5.models.heads.detect_v8 import \
@@ -57,9 +61,80 @@ class FlexibleYOLO(YOLOModel):
 
         model_config.head['ch'] = ch_in
 
-        if not isinstance(self.detection, Detect):
+        if head_cls != Detect:
             model_config.head.pop('anchors')
         self.detection = head_cls(**model_config.head)
+
+        if isinstance(self.detection, Detect):
+            s = 256  # 2x min stride
+            self.detection.stride = torch.tensor([s / x.shape[-2] for x in self.forward(torch.zeros(1, 3, s, s))])
+            self.detection.anchors /= self.detection.stride.view(-1, 1, 1)
+
+            self.stride = self.detection.stride
+            self._initialize_biases()  # only run once
+        if isinstance(self.detection, DetectV8):
+            s = 256  # 2x min stride
+            # self.detection.inplace = self.inplace
+            forward = lambda x: self.forward(x)
+            self.detection.stride = torch.tensor([s / x.shape[-2] for x in forward(torch.zeros(1, 3, s, s))])  # forward
+            self.stride = self.detection.stride
+            self.detection.bias_init()  # only run once
+
+        initialize_weights(self)
+
+    def forward(self, x):
+        out = self.backbone(x)
+        for neck in self.necks:
+            out = neck(out)
+        y = self.detection(list(out))
+        return y
+
+    def _apply(self, fn):
+        # Apply to(), cpu(), cuda(), half() to model tensors that are not parameters or registered buffers
+        self = nn.Module._apply(self, fn)
+        m = self.detection  # Detect()
+        if isinstance(m, Detect):
+            m.stride = fn(m.stride)
+            m.grid = list(map(fn, m.grid))
+            if isinstance(m.anchor_grid, list):
+                m.anchor_grid = list(map(fn, m.anchor_grid))
+        return self
+
+
+class YOLOv6(FlexibleYOLO):
+    def __init__(self, model_config, nc=80, custom_head=None):
+        """
+        :param model_config:
+        """
+        nn.Module.__init__(self)
+
+        head_config = {
+            'nc': nc,
+            'anchors':
+                (
+                 [10,13, 16,30, 33,23],
+                 [30,61, 62,45, 59,119],
+                 [116,90, 156,198, 373,326]
+                )
+        }
+
+        head_cls = Detect
+        if custom_head is not None:
+            head_cls = HEAD_NAME_MAP[custom_head]
+
+        cfg = Config.fromfile(model_config)
+        if not hasattr(cfg, 'training_mode'):
+            setattr(cfg, 'training_mode', 'repvgg')
+
+        self.backbone, self.neck, channel_counts = build_network(cfg)
+
+        self.necks = nn.ModuleList()
+        self.necks.append(self.neck)
+        head_config['ch'] = channel_counts
+
+        if head_cls != Detect:
+            head_config.pop('anchors')
+        self.detection = head_cls(**head_config)
 
         if isinstance(self.detection, Detect):
             s = 256  # 2x min stride
--- deeplite_torch_zoo/wrappers/models/objectdetection/__init__.py
+++ deeplite_torch_zoo/wrappers/models/objectdetection/__init__.py
@@ -2,3 +2,4 @@ from .flexible_yolo import *  # pylint: disable=unused-import
 from .mask_rcnn import *  # pylint: disable=unused-import
 from .ssd import *  # pylint: disable=unused-import
 from .yolo import *  # pylint: disable=unused-import
+from .yolov6 import *  # pylint: disable=unused-import
