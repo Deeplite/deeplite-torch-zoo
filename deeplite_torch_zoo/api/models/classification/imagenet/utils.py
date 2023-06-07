6db4fe45f3eeca75329c44a0406c102369501aa3
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2023-06-07 00:26:06 -0400

    add edgevit

--- deeplite_torch_zoo/api/models/classification/imagenet/utils.py
+++ deeplite_torch_zoo/api/models/classification/imagenet/utils.py
@@ -1,5 +1,5 @@
 from deeplite_torch_zoo.api.registries import MODEL_WRAPPER_REGISTRY
-
+from deeplite_torch_zoo.utils import load_pretrained_weights
 
 NUM_IMAGENET_CLASSES = 1000
 
@@ -28,3 +28,14 @@ def make_wrapper_func(
 
     wrapper_func.__name__ = wrapper_name
     return wrapper_func
+
+
+def load_checkpoint(model, model_name, dataset_name, model_urls, device):
+    if f"{model_name}_{dataset_name}" not in model_urls:
+        raise ValueError(
+            f'Could not find a pretrained checkpoint for model {model_name} on dataset {dataset_name}. \n'
+            'Use pretrained=False if you want to create a untrained model.'
+        )
+    checkpoint_url = model_urls[f"{model_name}_{dataset_name}"]
+    model = load_pretrained_weights(model, checkpoint_url, device)
+    return model
--- deeplite_torch_zoo/api/models/classification/imagenet/zoo_models.py
+++ deeplite_torch_zoo/api/models/classification/imagenet/zoo_models.py
@@ -1,15 +1,25 @@
-from deeplite_torch_zoo.src.classification.imagenet_models.mobileone import mobileone
-from deeplite_torch_zoo.utils import load_pretrained_weights
 from deeplite_torch_zoo.api.registries import MODEL_WRAPPER_REGISTRY
-from deeplite_torch_zoo.api.models.classification.imagenet.utils import make_wrapper_func, NUM_IMAGENET_CLASSES
+from deeplite_torch_zoo.api.models.classification.imagenet.utils import (
+    make_wrapper_func,
+    load_checkpoint,
+    NUM_IMAGENET_CLASSES,
+)
 
+from deeplite_torch_zoo.src.classification.imagenet_models.mobileone import mobileone
+from deeplite_torch_zoo.src.classification.imagenet_models.edgevit import (
+    edgevit_xxs,
+    edgevit_xs,
+    edgevit_s,
+)
 from torchvision.models import MobileNetV2
 
 __all__ = [
     'mobilenetv2_w035',
 ]
 
-MOBILEONE_BASE_URL = 'https://docs-assets.developer.apple.com/ml-research/datasets/mobileone'
+MOBILEONE_BASE_URL = (
+    'https://docs-assets.developer.apple.com/ml-research/datasets/mobileone'
+)
 CHECKPOINT_URLS = {
     'mobilenetv2_w035': 'http://download.deeplite.ai/zoo/models/mobilenetv2_w035_imagenet_6020_4a56477132807d76.pt',
     'mobileone_s0_zoo': f'{MOBILEONE_BASE_URL}/mobileone_s0_unfused.pth.tar',
@@ -20,33 +30,14 @@ CHECKPOINT_URLS = {
 }
 
 
-@MODEL_WRAPPER_REGISTRY.register(
-    model_name='mobilenetv2_w035_zoo',
-    dataset_name='imagenet',
-    task_type='classification',
-    has_checkpoint=True,
-)
-def mobilenetv2_w035(pretrained=False, device="cuda", num_classes=NUM_IMAGENET_CLASSES):
-    model = MobileNetV2(width_mult=0.35, num_classes=num_classes)
-
-    if pretrained:
-        checkpoint_url = CHECKPOINT_URLS['mobilenetv2_w035']
-        model = load_pretrained_weights(model, checkpoint_url, device)
-
-    return model.to(device)
-
-
 def get_mobileone(
     model_name, num_classes=NUM_IMAGENET_CLASSES, pretrained=False, device='cuda'
 ):
     model = mobileone(
         num_classes=num_classes, inference_mode=False, variant=model_name.split('_')[1]
     )
-
     if pretrained:
-        checkpoint_url = CHECKPOINT_URLS[model_name]
-        model = load_pretrained_weights(model, checkpoint_url, device)
-
+        model = load_checkpoint(model, model_name, 'imagenet', CHECKPOINT_URLS, device)
     return model.to(device)
 
 
@@ -55,3 +46,61 @@ for mobileone_variant in ('s0', 's1', 's2', 's3', 's4'):
     wrapper_tag = f'{model_tag}_imagenet'
     globals()[wrapper_tag] = make_wrapper_func(get_mobileone, wrapper_tag, model_tag)
     __all__.append(wrapper_tag)
+
+
+@MODEL_WRAPPER_REGISTRY.register(
+    model_name='mobilenetv2_w035_zoo',
+    dataset_name='imagenet',
+    task_type='classification',
+    has_checkpoint=True,
+)
+def mobilenetv2_w035(pretrained=False, device="cuda", num_classes=NUM_IMAGENET_CLASSES):
+    model = MobileNetV2(width_mult=0.35, num_classes=num_classes)
+    if pretrained:
+        model = load_checkpoint(
+            model, 'mobilenetv2_w035_zoo', 'imagenet', CHECKPOINT_URLS, device
+        )
+    return model.to(device)
+
+
+@MODEL_WRAPPER_REGISTRY.register(
+    model_name='edgevit_xxs',
+    dataset_name='imagenet',
+    task_type='classification',
+    has_checkpoint=False,
+)
+def get_edgevit_xxs(num_classes=NUM_IMAGENET_CLASSES, pretrained=False, device='cuda'):
+    model = edgevit_xxs(num_classes=num_classes)
+    if pretrained:
+        model = load_checkpoint(
+            model, 'edgevit_xxs', 'imagenet', CHECKPOINT_URLS, device
+        )
+    return model.to(device)
+
+
+@MODEL_WRAPPER_REGISTRY.register(
+    model_name='edgevit_xs',
+    dataset_name='imagenet',
+    task_type='classification',
+    has_checkpoint=False,
+)
+def get_edgevit_xs(num_classes=NUM_IMAGENET_CLASSES, pretrained=False, device='cuda'):
+    model = edgevit_xs(num_classes=num_classes)
+    if pretrained:
+        model = load_checkpoint(
+            model, 'edgevit_xs', 'imagenet', CHECKPOINT_URLS, device
+        )
+    return model.to(device)
+
+
+@MODEL_WRAPPER_REGISTRY.register(
+    model_name='edgevit_s',
+    dataset_name='imagenet',
+    task_type='classification',
+    has_checkpoint=False,
+)
+def get_edgevit_s(num_classes=NUM_IMAGENET_CLASSES, pretrained=False, device='cuda'):
+    model = edgevit_s(num_classes=num_classes)
+    if pretrained:
+        model = load_checkpoint(model, 'edgevit_s', 'imagenet', CHECKPOINT_URLS, device)
+    return model.to(device)
