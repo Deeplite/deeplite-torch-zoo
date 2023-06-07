23309194ca06e327f2a7f40b168729e708e97728
lzrwch <92095665+lzrwch@users.noreply.github.com>
2023-06-07 11:41:31 -0400

    clean up edgevit (#309)

--- deeplite_torch_zoo/api/models/classification/imagenet/utils.py
+++ deeplite_torch_zoo/api/models/classification/imagenet/utils.py
@@ -31,11 +31,9 @@ def make_wrapper_func(
 
 
 def load_checkpoint(model, model_name, dataset_name, model_urls, device):
-    if f"{model_name}_{dataset_name}" not in model_urls:
+    if model_name not in model_urls:
         raise ValueError(
             f'Could not find a pretrained checkpoint for model {model_name} on dataset {dataset_name}. \n'
             'Use pretrained=False if you want to create a untrained model.'
         )
-    checkpoint_url = model_urls[f"{model_name}_{dataset_name}"]
-    model = load_pretrained_weights(model, checkpoint_url, device)
-    return model
+    return load_pretrained_weights(model, model_urls[model_name], device)
