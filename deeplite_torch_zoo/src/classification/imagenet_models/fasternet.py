d8b11ef9319003918ecfbca2c0b9e1fb58df1a93
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2023-06-07 18:45:02 -0400

    add wrappers for fasternet

--- deeplite_torch_zoo/src/classification/imagenet_models/fasternet.py
+++ deeplite_torch_zoo/src/classification/imagenet_models/fasternet.py
@@ -1,3 +1,5 @@
+# Source: https://github.com/JierunChen/FasterNet
+
 from typing import List
 from torch import Tensor
 import copy
@@ -311,3 +313,117 @@ class FasterNet(nn.Module):
                 outs.append(x_out)
 
         return outs
+
+
+def fasternet_s(num_classes=1000):
+    model = FasterNet(
+        num_classes=num_classes,
+        mlp_ratio=2,
+        embed_dim=128,
+        depths=(1, 2, 13, 2),
+        feature_dim=1280,
+        patch_size=4,
+        patch_stride=4,
+        patch_size2=2,
+        patch_stride2=2,
+        layer_scale_init_value=0.0,
+        drop_path_rate=0.1,
+        act='relu',
+        n_div=4,
+    )
+    return model
+
+
+def fasternet_m(num_classes=1000):
+    model = FasterNet(
+        num_classes=num_classes,
+        mlp_ratio=2,
+        embed_dim=144,
+        depths=(3, 4, 18, 3),
+        feature_dim=1280,
+        patch_size=4,
+        patch_stride=4,
+        patch_size2=2,
+        patch_stride2=2,
+        layer_scale_init_value=0.0,
+        drop_path_rate=0.2,
+        act='relu',
+        n_div=4,
+    )
+    return model
+
+
+def fasternet_l(num_classes=1000):
+    model = FasterNet(
+        num_classes=num_classes,
+        mlp_ratio=2,
+        embed_dim=192,
+        depths=(3, 4, 18, 3),
+        feature_dim=1280,
+        patch_size=4,
+        patch_stride=4,
+        patch_size2=2,
+        patch_stride2=2,
+        layer_scale_init_value=0.0,
+        drop_path_rate=0.3,
+        act='relu',
+        n_div=4,
+    )
+    return model
+
+
+def fasternet_t0(num_classes=1000):
+    model = FasterNet(
+        num_classes=num_classes,
+        mlp_ratio=2,
+        embed_dim=40,
+        depths=(1, 2, 8, 2),
+        feature_dim=1280,
+        patch_size=4,
+        patch_stride=4,
+        patch_size2=2,
+        patch_stride2=2,
+        layer_scale_init_value=0.0,
+        drop_path_rate=0.0,
+        act='gelu',
+        n_div=4,
+    )
+    return model
+
+
+def fasternet_t1(num_classes=1000):
+    model = FasterNet(
+        num_classes=num_classes,
+        mlp_ratio=2,
+        embed_dim=64,
+        depths=(1, 2, 8, 2),
+        feature_dim=1280,
+        patch_size=4,
+        patch_stride=4,
+        patch_size2=2,
+        patch_stride2=2,
+        layer_scale_init_value=0.0,
+        drop_path_rate=0.02,
+        act='gelu',
+        n_div=4,
+    )
+    return model
+
+
+def fasternet_t2(num_classes=1000):
+    model = FasterNet(
+        num_classes=num_classes,
+        mlp_ratio=2,
+        embed_dim=96,
+        depths=(1, 2, 8, 2),
+        feature_dim=1280,
+        patch_size=4,
+        patch_stride=4,
+        patch_size2=2,
+        patch_stride2=2,
+        layer_scale_init_value=0.0,
+        drop_path_rate=0.05,
+        act='relu',
+        n_div=4,
+    )
+    return model
