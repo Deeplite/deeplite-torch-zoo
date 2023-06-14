af4a744ca5851e61ca00784ff5d3d3f799bcffd7
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2023-06-14 13:03:13 -0400

    fixes for OD script

--- deeplite_torch_zoo/utils/torch_utils.py
+++ deeplite_torch_zoo/utils/torch_utils.py
@@ -316,3 +316,19 @@ class no_jit_trace:
     def __exit__(self, *args):
         torch._C._set_tracing_state(self.state)
         self.state = None
+
+
+def strip_optimizer(f='best.pt', s=''):
+    # Strip optimizer from 'f' to finalize training, optionally save as 's'
+    x = torch.load(f, map_location=torch.device('cpu'))
+    if x.get('ema'):
+        x['model'] = x['ema']  # replace model with ema
+    for k in 'optimizer', 'training_results', 'wandb_id', 'ema', 'updates':  # keys
+        x[k] = None
+    x['epoch'] = -1
+    x['model'].half()  # to FP16
+    for p in x['model'].parameters():
+        p.requires_grad = False
+    torch.save(x, s or f)
+    mb = os.path.getsize(s or f) / 1E6  # filesize
+    LOGGER.info(f"Optimizer stripped from {f},{(' saved as %s,' % s) if s else ''} {mb:.1f}MB")
