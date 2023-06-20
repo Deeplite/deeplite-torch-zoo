23943b5c3902f8088cf68ed3f30e7cf265f76cda
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2023-06-20 00:50:00 -0400

    add pytorchcv checkpoint information

--- deeplite_torch_zoo/api/models/classification/imagenet/pytorchcv_models.py
+++ deeplite_torch_zoo/api/models/classification/imagenet/pytorchcv_models.py
@@ -2,7 +2,7 @@ from pytorchcv.model_provider import get_model as ptcv_get_model
 
 from deeplite_torch_zoo.utils import load_state_dict_partial
 from deeplite_torch_zoo.api.models.classification.model_implementation_dict import (
-    MODEL_IMPLEMENTATIONS,
+    MODEL_IMPLEMENTATIONS, PYTORCHCV_HAS_CHECKPOINT
 )
 from deeplite_torch_zoo.api.registries import MODEL_WRAPPER_REGISTRY
 from deeplite_torch_zoo.api.models.classification.imagenet.utils import NUM_IMAGENET_CLASSES
@@ -13,6 +13,7 @@ def make_wrapper_func(wrapper_fn_name, register_model_name_key, model_name_key):
         model_name=register_model_name_key,
         dataset_name='imagenet',
         task_type='classification',
+        has_checkpoint=register_model_name_key in PYTORCHCV_HAS_CHECKPOINT,
     )
     def wrapper_func(
         pretrained=False, num_classes=NUM_IMAGENET_CLASSES
