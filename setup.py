d0b20a3c3621a67ae23c369c7181da5cc1fd1efa
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2023-08-29 17:02:00 -0400

    bump version to 2.0.2

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="deeplite-torch-zoo",
-    version="2.0.1",
+    version="2.0.2",
     description="The deeplite-torch-zoo package is a collection of popular pretrained deep learning models and their datasets for PyTorch framework.",
     long_description=long_description,
     long_description_content_type="text/markdown",
