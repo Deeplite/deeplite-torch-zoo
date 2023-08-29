4d34bb9133e7f8685bcadf60f5cd6a339638d988
hossein-dl <mohammad@deeplite.ai>
2023-08-29 09:55:43 -0400

    Passing test and train length for CI

--- tests/test_raw_dataset_loading.py
+++ tests/test_raw_dataset_loading.py
@@ -16,13 +16,26 @@ BATCH_SIZE = 128
         ('pascalraw', DATASETS_ROOT, 40, 11),
     ],
 )
-def test_get_dataloaders(dataset_name, tmp_dataset_folder):
+def test_get_dataloaders(dataset_name, tmp_dataset_folder, train_dataloader_len, test_dataloader_len):
     p = Path(tmp_dataset_folder)
-    dataloaders = get_dataloaders(
+    dataloaders = get_pascal_raw(
         data_root=tmp_dataset_folder,
         dataset_name=dataset_name,
         batch_size=BATCH_SIZE,
         test_batch_size=BATCH_SIZE,
     )
+
+def get_pascal_raw(
+    data_root="",
+    batch_size=2,
+    test_batch_size=None,
+    num_workers=4,
+    fp16=False,
+    distributed=False,
+    train_transforms=None,
+    val_transforms=None,
+    input_size=[224, 224, 3],
+    **kwargs
+):
     assert len(dataloaders['train']) == train_dataloader_len
     assert len(dataloaders['test']) == test_dataloader_len
