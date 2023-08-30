5fd0a1a32826c41a6f643042bc3bcc04553374e8
Ravish Kumar <ravish@deeplite08.deeplite.local>
2023-08-30 10:51:18 -0400

    rectified missing checks for pytest

--- deeplite_torch_zoo/utils/torch_utils.py
+++ deeplite_torch_zoo/utils/torch_utils.py
@@ -800,6 +800,7 @@ def get_num_gradients(model):
     """Return the total number of parameters with gradients in a YOLO model."""
     return sum(x.numel() for x in model.parameters() if x.requires_grad)
 
+
 def fuse_blocks(model: torch.nn.Module) -> nn.Module:
     model = copy.deepcopy(model)
     for module in model.modules():
