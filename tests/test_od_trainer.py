79f40288b7d8f5d73cae71da89f77db35294e5d3
Ivan Lazarevich <ivan.lazarevich@deeplite.ai>
2023-09-04 22:58:17 -0400

    enable passing a custom model to yolo trainer

--- tests/test_od_trainer.py
+++ tests/test_od_trainer.py
@@ -1,5 +1,6 @@
 import pytest
 
+from deeplite_torch_zoo import get_model
 from deeplite_torch_zoo.trainer import Detector
 
 
@@ -14,4 +15,24 @@ from deeplite_torch_zoo.trainer import Detector
 )
 def test_detection_trainer(model_name):
     model = Detector(model_name=model_name)
+    model.train(data='coco8.yaml', epochs=1)
+
+
+@pytest.mark.parametrize(
+    ('model_name', ),
+    [
+        ('yolo7n', ),
+        ('yolo_fdresnet18x0.25', ),
+        ('yolo_timm_tinynet_e', ),
+        ('yolo6s-d33w25', ),
+    ],
+)
+def test_detection_trainer_torch_model(model_name):
+    torch_model = get_model(
+        model_name=model_name,
+        dataset_name='coco',
+        pretrained=False,
+        custom_head='v8',
+    )
+    model = Detector(torch_model=torch_model)
     model.train(data='coco8.yaml', epochs=1)
