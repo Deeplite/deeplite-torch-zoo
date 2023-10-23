35a61a2bfde6a171516cbf8f90581202ff201d3e
Ivan <92095665+lzrwch@users.noreply.github.com>
2023-10-22 20:48:25 -0400

    add v6.0-3 lite models (#367)

--- deeplite_torch_zoo/src/object_detection/yolo/flexible_yolo/yolov6/__init__.py
+++ deeplite_torch_zoo/src/object_detection/yolo/flexible_yolo/yolov6/__init__.py
@@ -64,3 +64,32 @@ def build_network(config, channels=3):
     chx = [6, 8, 10] if config.model.head.num_layers == 3 else [8, 9, 10, 11]
     head_channels = [channels_list[ch_idx] for ch_idx in chx]
     return backbone, neck, head_channels
+
+
+def build_network_lite(config, channels=3):
+    width_mul = config.model.width_multiple
+
+    num_repeat_backbone = config.model.backbone.num_repeats
+    out_channels_backbone = config.model.backbone.out_channels
+    scale_size_backbone = config.model.backbone.scale_size
+    in_channels_neck = config.model.neck.in_channels
+    unified_channels_neck = config.model.neck.unified_channels
+    in_channels_head = config.model.head.in_channels
+
+    BACKBONE = eval(config.model.backbone.type)
+    NECK = eval(config.model.neck.type)
+
+    out_channels_backbone = [make_divisible(i * width_mul, divisor=16)
+                            for i in out_channels_backbone]
+    mid_channels_backbone = [make_divisible(int(i * scale_size_backbone), divisor=8)
+                            for i in out_channels_backbone]
+    in_channels_neck = [make_divisible(i * width_mul, divisor=16)
+                       for i in in_channels_neck]
+
+    backbone = BACKBONE(channels,
+                        mid_channels_backbone,
+                        out_channels_backbone,
+                        num_repeat=num_repeat_backbone)
+    neck = NECK(in_channels_neck, unified_channels_neck)
+
+    return backbone, neck, in_channels_head
